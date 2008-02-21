5f257a6a82a079955d1af542ff6a855101eeaefc
Nicolas Pouillon <nipo@ssji.net>
2008-02-21 18:14:44 +0000

    Get soclib includes

--- arch/soclib/config.mk
+++ arch/soclib/config.mk
@@ -1,1 +1,7 @@
-ARCHCFLAGS=
+HAS_SOCLIB_CC:=$(shell (which soclib-cc 2>&1 >/dev/null && echo 1 || echo 0))
+ifeq ($(HAS_SOCLIB_CC),0)
+$(shell echo "SOCLIB builder \`soclib-cc' is not found, you'll experience build problems." 1>&2)
+else
+SOCLIB:=$(shell soclib-cc --getpath)
+endif
+ARCHCFLAGS=-I$(SOCLIB)/utils/include
