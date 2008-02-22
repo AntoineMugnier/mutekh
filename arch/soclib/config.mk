03eab5bbbac5dfc4ae681329009f43fac35e1979
Nicolas Pouillon <nipo@ssji.net>
2008-02-22 11:54:58 +0000

    Dont rehash soclib-cc in every subdir, try yo cache the result

--- arch/soclib/config.mk
+++ arch/soclib/config.mk
@@ -1,7 +1,18 @@
+ifeq ($(origin HAS_SOCLIB_CC),undefined)
+
 HAS_SOCLIB_CC:=$(shell (which soclib-cc 2>&1 >/dev/null && echo 1 || echo 0))
+export HAS_SOCLIB_CC
+
+ifeq ($(HAS_SOCLIB_CC),1)
+SOCLIB:=$(shell soclib-cc --getpath)
+export SOCLIB
+endif
+
+ARCHCFLAGS=-I$(SOCLIB)/utils/include
+export ARCHCFLAGS
+
+endif
+
 ifeq ($(HAS_SOCLIB_CC),0)
 $(shell echo "SOCLIB builder \`soclib-cc' is not found, you'll experience build problems." 1>&2)
-else
-SOCLIB:=$(shell soclib-cc --getpath)
 endif
-ARCHCFLAGS=-I$(SOCLIB)/utils/include
