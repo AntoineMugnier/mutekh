6b310833fd21d5ae149419888be72c4efdd4a646
Joel Porquet <joel.porquet@lip6.fr>
2009-01-21 14:35:14 +0000

    A microshell (ls, cat, cd, pwd) using lua and exploiting a filesystem via libc/libvfs.

---
+++ examples/lua_microshell/getline_lua.c
@@ -0,0 +1,191 @@
+#include <drivers/device/icu/soclib/icu-soclib.h>
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <hexo/device.h>
+#include <device/driver.h>
+#include <hexo/interrupt.h>
+
+#include <drivers/device/icu/soclib/icu-soclib.h>
+#include <drivers/device/block/soclib/block-soclib.h>
+
+#include <vfs/vfs.h>
+#include <lua.h>
+
+#include <termui/term.h>
+#include <termui/getline.h>
+
+extern struct device_s icu_dev;
+
+struct device_s bd_dev;
+
+extern struct device_s *tty_dev;
+
+struct vfs_node_s *vfs_root_term;
+
+extern struct stream_ops_s vfs_ops;
+
+pthread_t a;
+
+/* shell functions */
+int ls(lua_State*);
+int cat(lua_State*);
+int cd(lua_State*);
+int pwd(lua_State*);
+
+/* line completion handler found in getline_lua_complete.c */
+GETLINE_FCN_COMPLETE(lua_complete);
+
+static GETLINE_FCN_PROMPT(prompt)
+{
+    return term_printf(tm, "[lua:%s] ", vfs_root_term->n_name);
+}
+
+static void *l_alloc (void *ud, void *ptr, size_t osize, size_t nsize)
+{
+    void	*p;
+
+    (void)ud;
+    (void)osize;
+    if (nsize == 0) {
+        free(ptr);
+        p = NULL;
+    }
+    else
+        p = realloc(ptr, nsize);
+
+    return p;
+}
+
+typedef struct LoadS {
+    const char *s;
+    size_t size;
+} LoadS;
+
+static const char *getS (lua_State *L, void *ud, size_t *size) {
+    LoadS *ls = (LoadS *)ud;
+    (void)L;
+    if (ls->size == 0) return NULL;
+    *size = ls->size;
+    ls->size = 0;
+    return ls->s;
+}
+
+int luaL_loadbuffer (lua_State *L, const char *buff, size_t size,
+        const char *name) {
+    LoadS ls;
+    ls.s = buff;
+    ls.size = size;
+    return lua_load(L, getS, &ls, name);
+}
+
+void* shell(void *param)
+{
+    struct term_s			*tm;
+    struct term_behavior_s	*bhv;
+    lua_State			*luast;
+
+    printf("init vfs... ");
+    vfs_init(&bd_dev, VFS_VFAT_TYPE, 20, 20, &vfs_root_term);
+    stream_fops = &vfs_ops;
+    printf("ok\n");
+
+    /* create lua state */
+    luast = lua_newstate(l_alloc, NULL);
+
+    lua_register(luast, "ls", ls);
+    lua_register(luast, "cat", cat);
+    lua_register(luast, "cd", cd);
+    lua_register(luast, "pwd", pwd);
+
+    /* initialize terminal */
+    if (!(tm = term_alloc(tty_dev, tty_dev, luast)))
+        return -1;
+
+    /* set capabilities */
+    term_set(tm, "xterm");
+
+#if defined(CONFIG_DRIVER_CHAR_SOCLIBTTY)
+    char *disable_cr = "\x1b[20l";
+    char *enable_cr = "\x1b[20h";
+    term_writestr(tm, disable_cr, strlen(disable_cr));
+#endif
+
+    term_printf(tm, "lua shell example, use Ctrl-D to quit\n\n");
+
+    /* initialize getline behavior according to term capabilities */
+    if (!(bhv = getline_alloc(tm, 256)))	/* max line len = 256 */
+        return -1;
+
+    getline_history_init(bhv, 64); /* 64 entries max */
+    getline_complete_init(bhv, lua_complete);
+    getline_setprompt(bhv, prompt);
+
+    while (1)
+    {
+        int oldtop;
+        const char *line;
+
+        if (!(line = getline_process(bhv)))
+            break;
+
+        /* skip blank line */
+        if (!*(line += strspn(line, "\n\r\t ")))
+            continue;
+
+        getline_history_addlast(bhv);
+
+        if (luaL_loadbuffer(luast, line, strlen(line), ""))
+        {
+            term_printf(tm, "%91AParse error:%A %s\n", lua_tostring(luast, -1));
+            lua_pop(luast, 1);
+            continue;
+        }
+
+        oldtop = lua_gettop(luast);
+
+#if defined(CONFIG_DRIVER_CHAR_SOCLIBTTY)
+        term_writestr(tm, enable_cr, strlen(enable_cr));
+#endif
+        int err = lua_pcall(luast, 0, LUA_MULTRET, 0);
+#if defined(CONFIG_DRIVER_CHAR_SOCLIBTTY)
+        term_writestr(tm, disable_cr, strlen(disable_cr));
+#endif
+
+        if (err)
+        {
+            term_printf(tm, "%91AExecution error:%A %s\n", lua_tostring(luast, -1));
+            lua_pop(luast, 1);
+            continue;
+        }
+
+        lua_pop(luast, lua_gettop(luast) - oldtop + 1);
+
+        //term_printf(tm, "\n");
+    }
+
+    lua_close(luast);
+
+    /* free resources allocated by getline behavior */
+    getline_free(bhv);
+
+    /* free resources and restore terminal attributes */
+    term_free(tm);
+
+    return 0;
+}
+
+int main()
+{
+    device_init(&bd_dev);
+    bd_dev.addr[0] = 0x65200000;
+    bd_dev.irq = 2;
+    block_soclib_init(&bd_dev, &icu_dev, NULL);
+    DEV_ICU_BIND(&icu_dev, &bd_dev);
+
+    pthread_create(&a, NULL, shell, NULL);
+}
+
---
+++ examples/lua_microshell/shell_functions.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <lua.h>
+#include <vfs/vfs.h>
+
+extern struct vfs_node_s *vfs_root_term;
+
+static void compute_name(struct vfs_node_s *node, char *buf)
+{
+    if (!node->n_parent)
+        strcat(buf, node->n_name);
+    else
+    {
+        compute_name(node->n_parent, buf);
+        strcat(buf, node->n_name);
+        strcat(buf, "/");
+    }
+}
+
+static inline char * lua_getstringopt(lua_State *st)
+{
+    const char *s = lua_tolstring(st, 1, 0);
+    if (!s) 
+        lua_error(st);
+    return s;
+}
+
+int ls(lua_State *st)
+{
+    if (lua_gettop(st) <= 0)
+        lua_pushstring(st, ".");
+
+    const char *pathname = lua_getstringopt(st);
+
+    struct vfs_file_s *dir;
+    struct vfs_dirent_s dirent;
+
+    if (vfs_opendir(vfs_root_term, pathname, 0, &dir))
+    {
+        printf("no such file or directory\n");
+        return -1;
+    }
+
+    while (!vfs_readdir(dir, &dirent))
+        printf("%s  ", dirent.d_name);
+
+    printf("\n");
+
+    vfs_closedir(dir);
+
+    return 0;
+}
+
+int cat(lua_State *st)
+{
+    unsigned int i;
+
+    for (i = 1; i <= lua_gettop(st); i++)
+    {
+        switch (lua_type(st, i))
+        {
+            case LUA_TSTRING:
+                {
+                    FILE* f;
+                    char *filename = lua_tostring(st, i);
+                    char *pathname[128];
+                    char buffer[10];
+
+                    memset(pathname, 0, 128);
+                    if (filename[0] != '/') /* absolute path */
+                        compute_name(vfs_root_term, pathname);
+                    strncat(pathname, filename, 128-1-strlen(pathname));
+
+                    if ((f = fopen(pathname, "r")) == NULL)
+                    {
+                        printf("No such file %s\n", pathname);
+                        break;
+                    }
+                    while (fgets(buffer, sizeof(buffer), f))
+                        printf("%s", buffer);
+
+                    fclose(f);
+                    break;
+                }
+            default:
+                printf("bad argument\n");
+                break;
+        }
+    }
+
+    return 0;
+}
+
+int cd(lua_State *st)
+{
+    if (lua_gettop(st) <= 0)
+        lua_pushstring(st, ".");
+
+    const char *pathname = lua_getstringopt(st);
+
+    int err = vfs_chdir(pathname, vfs_root_term, &vfs_root_term);
+    if(err)
+    {
+        printf("no such file or directory\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+int pwd(lua_State *st)
+{
+    char *pathname[128];
+
+    memset(pathname, 0, 128);
+    compute_name(vfs_root_term, pathname);
+
+    printf("%s\n", pathname);
+
+    return 0;
+}
