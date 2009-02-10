5ad43e6eae0f612a8489a503c8ac0475872f7c8e
Alexandre Becoulet <alexandre.becoulet@free.fr>
2009-02-10 19:45:03 +0000

    quick start guide hello example added

---
+++ examples/hello/hello.c
@@ -0,0 +1,23 @@
+
+#include <pthread.h>
+
+pthread_mutex_t m;
+pthread_t a, b;
+
+void *f(void *param)
+{
+  while (1)
+    { 
+      pthread_mutex_lock(&m);
+      printf("(%i) %s", cpu_id(), param);
+      pthread_mutex_unlock(&m);
+      pthread_yield();
+    }
+}
+int main()
+{
+  pthread_mutex_init(&m, NULL);
+  pthread_create(&a, NULL, f, "Hello ");
+  pthread_create(&b, NULL, f, "World\n");
+}
+
