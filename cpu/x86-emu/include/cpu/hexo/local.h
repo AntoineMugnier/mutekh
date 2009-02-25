6715211fe725016347d11d76e2922a0fc2c198fe
Alexandre Becoulet <alexandre.becoulet@free.fr>
2009-02-25 18:14:07 +0000

    fixed emu binary loading issue with linux (x86_64)

--- cpu/x86-emu/include/cpu/hexo/local.h
+++ cpu/x86-emu/include/cpu/hexo/local.h
@@ -43,7 +43,6 @@
 /** context local storage type attribute */
 #define CONTEXT_LOCAL	__attribute__((section (".contextdata")))
 
-/** pointer to context local storage in cpu local storage */
 extern CPU_LOCAL void *__cpu_context_data_base;
 
 /* We do not have VMA to 0 for context local sections on this
