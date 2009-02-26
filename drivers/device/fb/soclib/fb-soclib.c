da0f00618c83003bd2598ea879a99f2d4eef0bee
Alexandre Becoulet <alexandre.becoulet@free.fr>
2009-02-26 16:36:35 +0000

    soclib frame buffer driver adde (just a hack yet)

---
+++ drivers/device/fb/soclib/fb-soclib.c
@@ -0,0 +1,120 @@
+/*
+    This file is part of MutekH.
+
+    MutekH is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    MutekH is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MutekH; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+    This driver is based on ssocliblib code (http://www.ssocliblib.org/)
+
+    Copyright Alexandre Becoulet <alexandre.becoulet@lip6.fr> (c) 2006
+
+*/
+
+#include <hexo/types.h>
+
+#include <device/fb.h>
+#include <hexo/device.h>
+#include <device/driver.h>
+
+#include <hexo/iospace.h>
+#include <hexo/alloc.h>
+#include <string.h>
+
+#include "fb-soclib.h"
+#include "fb-soclib-private.h"
+
+/* 
+ * device close operation
+ */
+
+DEV_CLEANUP(fb_soclib_cleanup)
+{
+  struct fb_soclib_context_s	*pv = dev->drv_pv;
+
+  //  mem_free(pv);
+}
+
+DEVFB_GETBUFFER(fb_soclib_getbuffer)
+{
+  return dev->addr[0];
+}
+
+DEVFB_SETMODE(fb_soclib_setmode)
+{
+  return 0;
+}
+
+DEVFB_FLIPPAGE(fb_soclib_flippage)
+{
+  uint8_t *flip = dev->addr[0] + 320*200 + 256*3;
+
+  *flip = 0;
+
+  return 0;
+}
+
+DEVFB_SETPALETTE(fb_soclib_setpalette)
+{
+  uint_fast16_t	i;
+  uint8_t *palette = dev->addr[0] + 320*200;
+
+  for (i = 0; i < __MIN(256, count); i++)
+    {
+      /* setup each channel */
+      cpu_mem_write_8(palette[i*3], pal[i].r);
+      cpu_mem_write_8(palette[i*3+1], pal[i].g);
+      cpu_mem_write_8(palette[i*3+2], pal[i].b);
+    }
+}
+
+/* 
+ * device open operation
+ */
+
+#ifndef CONFIG_STATIC_DRIVERS
+const struct driver_s	fb_soclib_drv =
+{
+  .class		= device_class_fb,
+  .f_init		= fb_soclib_init,
+  .f_cleanup		= fb_soclib_cleanup,
+  .f.fb = {
+    .f_setmode		= fb_soclib_setmode,
+    .f_getbuffer	= fb_soclib_getbuffer,
+    .f_flippage		= fb_soclib_flippage,
+    .f_setpalette	= fb_soclib_setpalette,
+  }
+};
+#endif
+
+DEV_INIT(fb_soclib_init)
+{
+  struct fb_soclib_context_s	*pv;
+
+#ifndef CONFIG_STATIC_DRIVERS
+  dev->drv = &fb_soclib_drv;
+#endif
+
+  /* alocate private driver data */
+#if 0
+  pv = mem_alloc(sizeof(*pv), MEM_SCOPE_SYS);
+
+  if (!pv)
+    return -1;
+#endif
+
+  dev->drv_pv = pv;
+
+  return 0;
+}
+
