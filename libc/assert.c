4d048215af4962e05ab794c68b1bb590aeb3ec30
Alexandre Becoulet <alexandre.becoulet@free.fr>
2009-03-17 15:29:58 +0000

    recusive assert() workaround added

--- libc/assert.c
+++ libc/assert.c
@@ -6,18 +6,22 @@
 
 ssize_t printk(const char *format, ...);
 
+bool_t failled = 0;
+
 void
 __assert_fail(const char *file,
 			  uint_fast16_t line,
 			  const char *func,
 			  const char *expr)
 {
-  printk("Assertion failed at %s:%u:%s(): (%s) is false\n", file, line, func, expr);
-
-  cpu_trap();
-
-  while (1)
-    ;
+  if (!failled)
+    {
+      failled = 1;
+      printk("Assertion failed at %s:%u:%s(): (%s) is false\n", file, line, func, expr);
+      cpu_trap();
+      while (1)
+	;
+    }
 }
 
 #endif
