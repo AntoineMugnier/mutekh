a4a070fe796be100c64862665db713349555f7ee
Joel Porquet <joel.porquet@lip6.fr>
2009-06-17 11:51:39 +0000

    __builtin_stdarg_start does not exist anymore in gcc 4.4.*

--- libc/include/stdarg.h
+++ libc/include/stdarg.h
@@ -35,7 +35,11 @@ typedef __builtin_va_list va_list;
  * @showcontent
  * @tt stdarg standard funtion
  */
-#define va_start(v,l)   __builtin_stdarg_start((v),l)
+#if defined(__GNUC__) && (__GNUC__ == 4 && __GNUC_MINOR__ >= 4 )
+    #define va_start(v, l) __builtin_va_start((v), (l)) 
+#else 
+    #define va_start(v, l) __builtin_stdarg_start((v), (l)) 
+#endif
 #define va_end          __builtin_va_end
 #define va_arg          __builtin_va_arg
 #define va_copy(d,s)    __builtin_va_copy((d),(s))
