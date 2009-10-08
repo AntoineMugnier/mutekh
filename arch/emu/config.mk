bccb424d2c4cf6a90b1913a3b4760a6e8f85c317
Nicolas Pouillon <nipo@ssji.net>
2009-10-08 10:52:17 +0000

    Set meaningful default flags for darwin EMU target

--- arch/emu/config.mk
+++ arch/emu/config.mk
@@ -1,10 +1,12 @@
 
+ARCHCFLAGS= -static
+ARCHLDFLAGS= -static
+
 ifeq ($(CONFIG_ARCH_EMU_DARWIN), defined)
 HOSTCPPFLAGS= -I/System/Library/Frameworks/Kernel.framework/Headers
 LD_NO_Q = 1
+ARCHCFLAGS+=-m32 -fno-pic
+CPUTOOLS=i686-unknown-elf-
 endif
-
-ARCHCFLAGS= -static
-ARCHLDFLAGS= -static
 
 
