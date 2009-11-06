2b15c26fb37e79e981863d35345b61bd83fcb239
Alexandre Becoulet <alexandre.becoulet@free.fr>
2009-11-06 17:13:02 +0000

    doc tweak

--- libsrl/include/srl/srl_sched_wait.h
+++ libsrl/include/srl/srl_sched_wait.h
@@ -9,30 +9,23 @@
 
 #include <stdint.h>
 
-#define DECLARE_WAIT(endianness, name, cmp)								\
-	void srl_sched_wait_##name##_##endianness( volatile uint32_t*addr, uint32_t val );
+#define DECLARE_WAIT(endian, name, cmp)				\
+/**									\
+   @this makes the current task sleep until the value pointed at @tt addr	\
+   asserts the following test:						\
+									\
+   @code								\
+   (*addr cmp val)							\
+   @end code								\
+									\
+   @tt addr is taken with the @b endian endianness.			\
+									\
+   @param addr The address to poll					\
+   @param val The value to compare to					\
+*/									\
+									\
+void srl_sched_wait_##name##_##endian( volatile uint32_t*addr, uint32_t val );
 
-/**
-   @multiple{18}
-   @this is of the form:
-
-   @code
-   srl_sched_wait_CMP_END(addr, val)
-   @end code
-
-   where @em CMP can be eq, ne, le, ge, lt, or gt, and @em END can be le, be, or cpu.
-
-   They make the current task sleep until the valut pointed at @tt addr
-   asserts the following test:
-   @code
-   (*addr CMP val)
-   @end code
-
-   @tt addr is taken with the END endianness (little, big, or the currrent cpu's one)
-
-   @param addr The address to poll
-   @param val The value to compare to
-*/
 DECLARE_WAIT(le, eq, ==)
 DECLARE_WAIT(le, ne, !=)
 DECLARE_WAIT(le, le, <=)
