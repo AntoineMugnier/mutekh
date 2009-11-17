e1db836b5337483a4671a68a359657cee7909316
Nicolas Pouillon <nipo@ssji.net>
2009-11-17 16:09:12 +0000

    Make GCC shut up, explicitely

--- drivers/device/device.c
+++ drivers/device/device.c
@@ -97,6 +97,14 @@ device_dump_list(struct device_s *root)
 
 #endif
 
+/*
+  I'm tired of this warning about uninitialized ap, so please, GCC,
+  shut up.
+ */
+#if defined(CONFIG_HEXO_DEVICE_TREE) && defined(__GNUC__)
+#pragma GCC diagnostic ignored "-Wuninitialized"
+#endif
+
 void
 device_init(struct device_s *dev)
 {
