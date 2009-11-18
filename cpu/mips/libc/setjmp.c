2c921e7c8d5f89aca7b61a53f18a77ccc4ac3227
Joel Porquet <joel.porquet@lip6.fr>
2009-11-18 12:53:56 +0000

    Reworked setjmp/longjmp for mips

--- cpu/mips/libc/setjmp.c
+++ cpu/mips/libc/setjmp.c
@@ -16,78 +16,81 @@
     Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
     Copyright Alexandre Becoulet <alexandre.becoulet@lip6.fr> (c) 2006
+    Copyright Joel Porquet <joel.porquet@lip6.fr> (c) 2009
 
 */
 
 #include <setjmp.h>
 
-reg_t setjmp(jmp_buf env);
-asm(
-        ".set push				\n"
-        ".set noat				\n"
-        ".globl setjmp          \n"
-        ".func setjmp           \n"
-        ".type setjmp, %function\n"
-        "setjmp:                \n"
-        /* save gp registers */
-        "sw     $16,    16*4($4)\n"
-        "sw     $17,    17*4($4)\n"
-        "sw     $18,    18*4($4)\n"
-        "sw     $19,    19*4($4)\n"
-        "sw     $20,    20*4($4)\n"
-        "sw     $21,    21*4($4)\n"
-        "sw     $22,    22*4($4)\n"
-        "sw     $23,    23*4($4)\n"
+reg_t setjmp(jmp_buf env)
+{
+    register reg_t ret;
 
-        "sw     $24,    24*4($4)\n"
-        "sw     $25,    25*4($4)\n"
+    asm volatile(
+            ".set push				\n"
+            ".set noat				\n"
+            /* save gp registers */
+            "sw     $16,    16*4(%[env])\n"
+            "sw     $17,    17*4(%[env])\n"
+            "sw     $18,    18*4(%[env])\n"
+            "sw     $19,    19*4(%[env])\n"
+            "sw     $20,    20*4(%[env])\n"
+            "sw     $21,    21*4(%[env])\n"
+            "sw     $22,    22*4(%[env])\n"
+            "sw     $23,    23*4(%[env])\n"
 
-        "sw     $gp,    28*4($4)\n"
-        "sw     $sp,    29*4($4)\n"
-        "sw     $fp,    30*4($4)\n"
-        "sw     $ra,    31*4($4)\n"
+            "sw     $24,    24*4(%[env])\n"
+            "sw     $25,    25*4(%[env])\n"
 
-        /* return is 0 */
-        "move   $2,     $0      \n"
-        "jr     $ra             \n"
-        ".set pop			    \n"
-		".endfunc               \n"
-        ".size setjmp, .-setjmp \n"
-       );
+            "sw     $gp,    28*4(%[env])\n"
+            "sw     $sp,    29*4(%[env])\n"
+            "sw     $fp,    30*4(%[env])\n"
+            "sw     $ra,    31*4(%[env])\n"
 
-void longjmp(jmp_buf env, reg_t val);
-asm(
-        ".set push				\n"
-        ".set noat				\n"
-        ".globl longjmp         \n"
-        ".func longjmp          \n"
-        ".type longjmp, %function\n"
-        "longjmp:               \n"
-        /* restore gp registers */
-        "lw     $16,    16*4($4)\n"
-        "lw     $17,    17*4($4)\n"
-        "lw     $18,    18*4($4)\n"
-        "lw     $19,    19*4($4)\n"
-        "lw     $20,    20*4($4)\n"
-        "lw     $21,    21*4($4)\n"
-        "lw     $22,    22*4($4)\n"
-        "lw     $23,    23*4($4)\n"
+            /* return is 0 */
+            "move   %[ret], $0      \n"
+            ".set pop			    \n"
+            : [ret] "=&r" (ret)
+            : [env] "r" (env)
+            : "memory"
+            );
 
-        "lw     $24,    24*4($4)\n"
-        "lw     $25,    25*4($4)\n"
+    return ret;
+}
 
-        "lw     $gp,    28*4($4)\n"
-        "lw     $sp,    29*4($4)\n"
-        "lw     $fp,    30*4($4)\n"
-        "lw     $ra,    31*4($4)\n"
+void longjmp(jmp_buf env, reg_t val)
+{
+    /* val must not be zero */
+    val = (!val) ? 1 : val;
 
-        /* set return value: */
-        "move   $2,     $5      \n"
-        "bnez   $5,     1f      \n"
-        "li     $2,     1       \n"
-        "1:                     \n"
-        "jr     $ra             \n"
-        ".set pop               \n"
-		".endfunc               \n"
-        ".size longjmp, .-longjmp\n"
-    );
+    asm volatile(
+            ".set push				\n"
+            ".set noat				\n"
+            /* restore gp registers */
+            "lw     $16,    16*4(%[env])\n"
+            "lw     $17,    17*4(%[env])\n"
+            "lw     $18,    18*4(%[env])\n"
+            "lw     $19,    19*4(%[env])\n"
+            "lw     $20,    20*4(%[env])\n"
+            "lw     $21,    21*4(%[env])\n"
+            "lw     $22,    22*4(%[env])\n"
+            "lw     $23,    23*4(%[env])\n"
+
+            "lw     $24,    24*4(%[env])\n"
+            "lw     $25,    25*4(%[env])\n"
+
+            "lw     $gp,    28*4(%[env])\n"
+            "lw     $sp,    29*4(%[env])\n"
+            "lw     $fp,    30*4(%[env])\n"
+            "lw     $ra,    31*4(%[env])\n"
+
+            /* set return value: */
+            "move   $2,     %[val]  \n"
+            "jr     $ra             \n"
+            ".set pop               \n"
+            :
+            : [env] "r" (env)
+            , [val] "r" (val)
+            : "memory"
+            );
+}
