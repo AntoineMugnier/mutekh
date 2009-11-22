e15e37de30fd2d383b42ab7687ee45e0d3b1fb5c
Nicolas Pouillon <nipo@ssji.net>
2009-11-22 21:41:39 +0000

    Add a quirk to detect arm GCC version and change flags accordingly

--- cpu/arm/config.mk
+++ cpu/arm/config.mk
@@ -2,6 +2,14 @@ CPUCFLAGS= -fsigned-char
 CPULDFLAGS=
 CPUASFLAGS=
 
+CPUTOOLS_GCC_VERSION:=$(shell \
+	(echo '((' ; $(CC) -dumpversion | sed -e 's:\.)\?:)*100+:g' ) \
+	)
+CPUTOOLS_GCC_VERSION:=$(shell echo "$(CPUTOOLS_GCC_VERSION)" | bc -q)
+
+# Dont forget the shell's 'true' is 0...
+CPUTOOLS_GCC_4_4_PLUS:=$(shell test "$(CPUTOOLS_GCC_VERSION)" -lt "40400" ; echo "$$?" )
+
 ifneq ($(CONFIG_CPU_ARM_CPU),undefined)
 CPUCFLAGS+= -mcpu=$(CONFIG_CPU_ARM_CPU)
 endif
@@ -11,7 +19,9 @@ CPUCFLAGS+= -march=$(CONFIG_CPU_ARM_ARCH)
 endif
 
 ifeq ($(CONFIG_COMPILE_DEBUG), defined)
+ifeq ($(CPUTOOLS_GCC_4_4_PLUS), 1)
 CPUCFLAGS += -fno-dwarf2-cfi-asm
+endif
 endif
 
 ifeq ($(CONFIG_CPU_ARM_FPU),soft)
