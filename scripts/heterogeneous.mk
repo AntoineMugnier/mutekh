0136429864b3f6856890f80a8a79878af0d24a1b
Nicolas Pouillon <nipo@ssji.net>
2010-01-15 16:03:37 +0000

    Heterogeneous build support

--- scripts/heterogeneous.mk
+++ scripts/heterogeneous.mk
@@ -1,28 +1,18 @@
 
 HETLINK=$(MUTEK_SRC_DIR)/tools/hlink/hetlink
-CONFS:=$(subst :, ,$(CONF))
+BUILDS:=$(subst :, ,$(BUILD))
 COUPLES:=
 
-define decl_conf
+$(eval $(foreach c,$(BUILDS),$(call decl_conf,$(c))))
 
-# Ugly Make things to make this work when using make -[pnd]
-COUPLE:=$(shell unset MAKEFLAGS ; $(MAKE_COMMAND) -s -f $(MUTEK_SRC_DIR)/scripts/config.mk couple CONF=$(1))
-COUPLES+=$$(COUPLE)
-$$(COUPLE)_CONF:=$(1)
-
-endef
-
-$(eval $(foreach c,$(CONFS),$(call decl_conf,$(c))))
-
-PRE_OBJS=$(foreach couple,$(COUPLES),$(BUILD_DIR)/kernel-$(couple).pre.o)
-HET_OBJS=$(foreach couple,$(COUPLES),$(BUILD_DIR)/kernel-$(couple).pre.o.het.o)
-HET_KERNELS=$(foreach couple,$(COUPLES),$(BUILD_DIR)/kernel-$(couple).het.out)
+PRE_OBJS=$(foreach build,$(BUILDS),$(BUILD_DIR)/kernel-$(build).pre.o)
+HET_OBJS=$(foreach build,$(BUILDS),$(BUILD_DIR)/kernel-$(build).pre.o.het.o)
+HET_KERNELS=$(foreach build,$(BUILDS),$(BUILD_DIR)/kernel-$(build).het.out)
 
 export HETLINK
 
 kernel-het: $(HET_KERNELS)
-	echo "TARGETS: $(TARGETS)"
-	echo "COUPLES: $(COUPLES)"
+	echo "BUILDS: $(BUILDS)"
 	echo "HET_KERNELS: $(HET_KERNELS)"
 	echo "PRE_OBJS: $(PRE_OBJS)"
 	echo "HET_OBJS: $(HET_OBJS)"
@@ -30,9 +20,10 @@ kernel-het: $(HET_KERNELS)
 $(BUILD_DIR)/kernel-%.pre.o: FORCE
 	@echo "PRE $@"
 	$(MAKE) -f $(MUTEK_SRC_DIR)/scripts/rules_main.mk \
-		 MAKEFLAGS=$(MAKEFLAGS) CONF=$($(*)_CONF) \
+		 MAKEFLAGS=$(MAKEFLAGS) CONF=$(CONF) \
+	     BUILD=$* \
 		 BUILD_DIR=$(BUILD_DIR) TARGET_EXT=pre.o \
-		 CONF_DIR=$(BUILD_DIR)/obj-$* \
+		 CONF_DIR=$(BUILD_DIR)/obj-$* target=kernel-$* \
 		 kernel
 
 # We have to go through an unique target or the hetlink will be done
@@ -48,9 +39,10 @@ __do_hetlink : $(PRE_OBJS) $(HETLINK_CONF) FORCE
 
 $(BUILD_DIR)/kernel-%.het.out : $(BUILD_DIR)/kernel-%.pre.o.het.o FORCE
 	$(MAKE) -f $(MUTEK_SRC_DIR)/scripts/rules_main.mk \
-		 MAKEFLAGS=$(MAKEFLAGS) CONF=$($(*)_CONF) \
+		 MAKEFLAGS=$(MAKEFLAGS) CONF=$(CONF) \
 		 BUILD_DIR=$(BUILD_DIR) \
 		 CONF_DIR=$(BUILD_DIR)/obj-$* \
+		 BUILD=$* \
 		 FINAL_LINK_TARGET=$@ \
 		 FINAL_LINK_SOURCE=$< \
 		 final_link
