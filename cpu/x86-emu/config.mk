15e4bceb37a873d429ac233b2d366ee8680652ed
Alexandre Becoulet <alexandre.becoulet@free.fr>
2010-02-24 21:13:20 +0000

    fixed x86 emu32 build with 64bits compiler

--- cpu/x86-emu/config.mk
+++ cpu/x86-emu/config.mk
@@ -1,9 +1,10 @@
 
-CPUCFLAGS=-mno-tls-direct-seg-refs -m32
-CPULDFLAGS=
-
 ifeq ($(CONFIG_ARCH_EMU_DARWIN), defined)
 CPUCFLAGS+= -fno-stack-protector -m32
 else
 CPUTOOLS=
 endif
+
+CPUCFLAGS=-mno-tls-direct-seg-refs -m32
+CPULDFLAGS=-melf_i386
+
