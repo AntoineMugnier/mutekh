7b496acaa3d542120da0e5c8a4d8f412b6dd42ee
Dimitri Refauvelet <dimitri.refauvelet@lip6.fr>
2010-07-27 23:11:39 +0000

    fix realloc: disable irq during memcpy

--- libc/alloc.c
+++ libc/alloc.c
@@ -55,11 +55,13 @@ realloc(void *ptr, size_t size)
   /*Memchecker must be disable for init checking. Initial area may
     contains uninitialized words.*/
 #ifdef CONFIG_SOCLIB_MEMCHECK
+  CPU_INTERRUPT_SAVESTATE_DISABLE;
   soclib_mem_check_disable(SOCLIB_MC_CHECK_INIT);
 #endif
-  //  memcpy(p, ptr, oldsize );
+  memcpy(p, ptr, oldsize );
 #ifdef CONFIG_SOCLIB_MEMCHECK
   soclib_mem_check_enable(SOCLIB_MC_CHECK_INIT);
+  CPU_INTERRUPT_RESTORESTATE;
 #endif
 
   free(ptr);
