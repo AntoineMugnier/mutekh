56878cd8a68e8ea2eccc3f91553ed3d34500294f
Eric Guthmuller <eric.guthmuller@polytechnique.edu>
2010-11-18 10:26:31 +0100

    Modify the hello example

--- examples/hello/hello.c
+++ examples/hello/hello.c
@@ -1,12 +1,23 @@
 
+#include <hexo/init.h>
+#include <hexo/types.h>
 #include <pthread.h>
 #include <mutek/printk.h>
 #include <arch/lm3s/lm3s8962.h>
+#include <drivers/icu/cm3/icu-cm3.h>
+#include <drivers/icu/arm/icu-arm.h>
+
+#include <device/device.h>
+#include <device/driver.h>
 
 #define THREAD_COUNT 4
 
 pthread_mutex_t m;
 pthread_t pthread[THREAD_COUNT];
+
+struct device_s icu_dev;
+struct device_s cpu_icu_dev;
+
 
 void *f(void *param)
 {
@@ -25,12 +36,35 @@ void *f(void *param)
     }
 }
 
+void say_yo(void *data){
+  printk("YO !!!\n");
+  LM3S_BASE_GPIOF->GPIO_ICRR = (1<<1); // clear interrupt
+}
+
 void app_start()
 {
   size_t i;
+  device_init(&cpu_icu_dev);
+  icu_arm_init(&cpu_icu_dev,NULL);
+
+  cpu_interrupt_sethandler_device(&cpu_icu_dev);
+
+  device_init(&icu_dev);
+  icu_dev.addr[0] = (uintptr_t)CM3_BASE_NVIC;
+  icu_dev.irq=0;
+  icu_dev.icudev = CPU_LOCAL_ADDR(cpu_icu_dev);
+  icu_cm3_init(&icu_dev, NULL);
+  
+  dev_icu_sethndl(&icu_dev,30,say_yo,NULL);
+  dev_icu_enable(&icu_dev,30,1,0);
+
+  cpu_interrupt_enable();
 
   pthread_mutex_init(&m, NULL);
-  for ( i = 0; i < THREAD_COUNT; ++i )
-    pthread_create(&pthread[i], NULL, f, "Hello world\n");
+  //  for ( i = 0; i < THREAD_COUNT; ++i )
+  pthread_create(&pthread[0], NULL, f, "Hello world, Thread 0\n");
+  pthread_create(&pthread[1], NULL, f, "Hello world, Thread 1\n");
+  pthread_create(&pthread[2], NULL, f, "Hello world, Thread 2\n");
+  pthread_create(&pthread[3], NULL, f, "Hello world, Thread 3\n");
 }
 
