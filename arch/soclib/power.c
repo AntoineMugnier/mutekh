46002817f452c23cab7c816dbdd6b742c884db48
Alexandre Becoulet <alexandre.becoulet@free.fr>
2011-04-19 10:01:46 +0200

    added hexo/power.h

---
+++ arch/soclib/power.c
@@ -0,0 +1,38 @@
+/*
+    This file is part of MutekH.
+    
+    MutekH is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; version 2.1 of the
+    License.
+    
+    MutekH is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+    
+    You should have received a copy of the GNU Lesser General Public
+    License along with MutekH; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301 USA.
+
+    Copyright Alexandre Becoulet <alexandre.becoulet@lip6.fr> (c) 2011
+
+*/
+
+#include <hexo/power.h>
+#include <hexo/iospace.h>
+
+error_t power_reboot()
+{
+  return ENOTSUP;
+}
+
+error_t power_shutdown()
+{
+  /* FIXME vci simhelper hack */
+  cpu_mem_write_32(0xd3200004, 0);
+
+  return 0;
+}
+
