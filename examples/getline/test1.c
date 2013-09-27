b7a22d3d9391a4577255ae8fdadb806a6b5e4c15
Alexandre Becoulet <alexandre.becoulet@free.fr>
2013-09-27 17:14:49 +0200

    moved to rev 27 of libtermui

--- examples/getline/test1.c
+++ examples/getline/test1.c
@@ -1,10 +1,11 @@
 
 #include <stdlib.h>
+
 #include <termui/term.h>
 #include <termui/getline.h>
-#include <device/class/char.h>
+#include <termui/mutekh.h>
 
-extern struct device_char_s console_dev;
+#include <mutek/console.h>
 
 static TERMUI_GETLINE_FCN_PROMPT(prompt)
 {
@@ -13,48 +14,41 @@ static TERMUI_GETLINE_FCN_PROMPT(prompt)
 
 void main()
 {
-  struct termui_term_s			*tm;
-  struct termui_term_behavior_s	*bhv;
+  struct termui_term_s tm;
+  struct termui_term_behavior_s	bhv;
 
-  device_dump_tree(0);
+  assert(device_check_accessor(&console_dev));
 
-  /* initialize terminal */
-  if (!(tm = termui_term_alloc(&console_dev, &console_dev, NULL)))
-    abort();
+  termui_dev_io_init(&tm, &console_dev, "xterm");
+  termui_getline_init(&tm, &bhv, 256);
+  termui_term_set_private(&tm, NULL);
 
-  /* set capabilities */
-  termui_term_set(tm, "xterm");
+  termui_term_printf(&tm, "libtermui getline example, use Ctrl-D to quit\n\n");
 
-  termui_term_printf(tm, "libtermui getline example, use Ctrl-D to quit\n\n");
-
-  /* initialize getline behavior according to term capabilities */
-  if (!(bhv = termui_getline_alloc(tm, 256)))	/* max line len = 256 */
-    abort();
-
-  termui_getline_history_init(bhv, 64); /* 64 entries max */
-  termui_getline_setprompt(bhv, prompt);
+  termui_getline_history_init(&bhv, 64); /* 64 entries max */
+  termui_getline_setprompt(&bhv, prompt);
 
   while (1)
     {
       const char *line;
 
-      if (!(line = termui_getline_process(bhv)))
+      if (!(line = termui_getline_process(&bhv)))
 	break;
 
       /* skip blank line */
       if (!*(line += strspn(line, "\n\r\t ")))
 	continue;
 
-      termui_getline_history_addlast(bhv);
+      termui_getline_history_addlast(&bhv);
 
-      termui_term_printf(tm, "entered line is: `%s'\n\n", line);
+      termui_term_printf(&tm, "entered line is: `%s'\n\n", line);
     }
 
   /* free resources allocated by getline behavior */
-  termui_getline_free(bhv);
+  termui_getline_cleanup(&bhv);
 
   /* free resources and restore terminal attributes */
-  termui_term_free(tm);
+  termui_termio_cleanup(&tm);
 
   while (1)
     ;
--- examples/getline_lua/getline_lua.c
+++ examples/getline_lua/getline_lua.c
@@ -24,6 +24,7 @@
  */
 
 #include <lua/lauxlib.h>
+#include <lua/lualib.h>
 #include <lua/lua.h>
 
 #include <stdlib.h>
@@ -31,61 +32,57 @@
 
 #include <termui/term.h>
 #include <termui/getline.h>
+#include <termui/mutekh.h>
+
+#include <mutek/console.h>
 
 /* line completion handler found in getline_lua_complete.c */
-GETLINE_FCN_COMPLETE(lua_complete);
+TERMUI_GETLINE_FCN_COMPLETE(lua_complete);
 
-static GETLINE_FCN_PROMPT(prompt)
+static TERMUI_GETLINE_FCN_PROMPT(prompt)
 {
-  return term_printf(tm, "[%31Alua%A] ");
+  return termui_term_printf(tm, "[%31Alua%A] ");
 }
 
-extern struct device_s *console_dev;
-
-int main()
+void main()
 {
-  struct term_s			*tm;
-  struct term_behavior_s	*bhv;
+  struct termui_term_s tm;
+  struct termui_term_behavior_s	bhv;
   lua_State			*luast;
+
+  assert(device_check_accessor(&console_dev));
 
   /* create lua state */
   luast = luaL_newstate();
   luaL_openlibs(luast);
 
-  /* initialize terminal */
-  if (!(tm = term_alloc(console_dev, console_dev, luast)))
-    return -1;
+  termui_dev_io_init(&tm, &console_dev, "xterm");
+  termui_getline_init(&tm, &bhv, 256);
+  termui_term_set_private(&tm, luast);
 
-  /* set capabilities */
-  term_set(tm, "xterm");
+  termui_term_printf(&tm, "libtermui lua getline example, use Ctrl-D to quit\n\n");
 
-  term_printf(tm, "libtermui lua getline example, use Ctrl-D to quit\n\n");
-
-  /* initialize getline behavior according to term capabilities */
-  if (!(bhv = getline_alloc(tm, 256)))	/* max line len = 256 */
-    return -1;
-
-  getline_history_init(bhv, 64); /* 64 entries max */
-  getline_complete_init(bhv, lua_complete);
-  getline_setprompt(bhv, prompt);
+  termui_getline_history_init(&bhv, 64); /* 64 entries max */
+  termui_getline_complete_init(&bhv, lua_complete);
+  termui_getline_setprompt(&bhv, prompt);
 
   while (1)
     {
       int oldtop;
       const char *line;
 
-      if (!(line = getline_process(bhv)))
+      if (!(line = termui_getline_process(&bhv)))
 	break;
 
       /* skip blank line */
       if (!*(line += strspn(line, "\n\r\t ")))
 	continue;
 
-      getline_history_addlast(bhv);
+      termui_getline_history_addlast(&bhv);
 
       if (luaL_loadbuffer(luast, line, strlen(line), ""))
 	{
-	  term_printf(tm, "%91AParse error:%A %s\n", lua_tostring(luast, -1));
+	  termui_term_printf(&tm, "%91AParse error:%A %s\n", lua_tostring(luast, -1));
 	  lua_pop(luast, 1);
 	  continue;
 	}
@@ -94,24 +91,22 @@ int main()
 
       if (lua_pcall(luast, 0, LUA_MULTRET, 0))
 	{
-	  term_printf(tm, "%91AExecution error:%A %s\n", lua_tostring(luast, -1));
+	  termui_term_printf(&tm, "%91AExecution error:%A %s\n", lua_tostring(luast, -1));
 	  lua_pop(luast, 1);
 	  continue;
 	}
 
       lua_pop(luast, lua_gettop(luast) - oldtop + 1);
 
-      term_printf(tm, "\n");
+      termui_term_printf(&tm, "\n");
     }
 
   lua_close(luast);
 
   /* free resources allocated by getline behavior */
-  getline_free(bhv);
+  termui_getline_cleanup(&bhv);
 
   /* free resources and restore terminal attributes */
-  term_free(tm);
-
-  return 0;
+  termui_termio_cleanup(&tm);
 }
 
