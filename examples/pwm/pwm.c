c97ec3863b11de276d4d18ed596992171647f29c
Julien Peeters <contact@julienpeeters.net>
2014-09-16 00:50:38 +0200

    examples: port pwm example to pwm class update

--- examples/pwm/pwm.c
+++ examples/pwm/pwm.c
@@ -8,7 +8,7 @@ void main()
   device_dump_tree(0);
 
   struct device_pwm_s pwm;
-  if (device_get_accessor_by_path(&pwm, 0, "/pwm3", DRIVER_CLASS_PWM))
+  if (device_get_accessor_by_path(&pwm, 0, "/pwm2", DRIVER_CLASS_PWM))
     {
       printk("pwm: error, cannot get accessor.\n");
       goto err_accessor;
@@ -19,7 +19,7 @@ void main()
   freq.num   = 480000;
   freq.denom = 1;
 
-  if (DEVICE_OP(&pwm, freq, &freq))
+  if (DEVICE_OP(&pwm, freq, &freq, NULL))
     {
       printk("pwm: error, cannot set PWM frequency.\n");
       goto err_accessor;
@@ -27,23 +27,23 @@ void main()
 
   struct dev_pwm_fract_s duty;
   duty.num   = 1;
-  duty.denom = 2;
+  duty.denom = 4;
 
-  if (DEVICE_OP(&pwm, duty, 0, &duty))
+  if (DEVICE_OP(&pwm, duty, 0, &duty, NULL))
     {
       printk("pwm: error, cannot set PWM duty cycle on channel 0.\n");
       goto err_accessor;
     }
 
   enum dev_pwm_polarity_e pol = DEV_PWM_POL_HIGH;
-  if (DEVICE_OP(&pwm, polarity, 0, &pol))
+  if (DEVICE_OP(&pwm, polarity, 0, &pol, NULL))
     {
       printk("pwm: error, cannot set PWM polarity on channel 0.\n");
       goto err_accessor;
     }
 #endif
 
-  if (DEVICE_OP(&pwm, start_stop, 0 /* channel */, 1 /* start */))
+  if (DEVICE_OP(&pwm, start_stop, 0 /* channel */, 1 /* start */, NULL))
     printk("pwm: error, cannot start PWM.\n");
 
 err_accessor:
