6ad65d92094a2f6be7586de80210d7fd1c655e25
Alexandre Becoulet <alexandre.becoulet@free.fr>
2014-10-07 02:02:16 +0200

    updated the soclib block device driver to use the new memory class instead of the block class

--- arch/soclib/drivers/vci_block_device/block.c
+++ arch/soclib/drivers/vci_block_device/block.c
@@ -16,7 +16,7 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
     02110-1301 USA.
 
-    Copyright Alexandre Becoulet <alexandre.becoulet@lip6.fr> (c) 2006
+    Copyright Alexandre Becoulet <alexandre.becoulet@free.fr> (c) 2014
 
 */
 
@@ -24,7 +24,7 @@
 
 #include <hexo/types.h>
 
-#include <device/class/block.h>
+#include <device/class/mem.h>
 #include <device/device.h>
 #include <device/resources.h>
 #include <device/class/icu.h>
@@ -41,14 +41,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct soclib_block_context_s
-{
-  uintptr_t                 addr;
-  struct dev_irq_ep_s       irq_ep;
-  struct dev_block_params_s params;
-  dev_blk_queue_root_t      queue;
-};
-
 #define SOCLIB_BLOCK_BUFFER 0
 #define SOCLIB_BLOCK_LBA 4
 #define SOCLIB_BLOCK_COUNT 8
@@ -57,106 +49,234 @@ struct soclib_block_context_s
 #define SOCLIB_BLOCK_IRQ_ENABLE 20
 #define SOCLIB_BLOCK_SIZE 24
 #define SOCLIB_BLOCK_BLOCK_SIZE 28
+#define SOCLIB_BLOCK_TYPE 32
+#define SOCLIB_BLOCK_OFFSET 36
 
-#define SOCLIB_BLOCK_OP_NOOP 0
-#define SOCLIB_BLOCK_OP_READ 1
-#define SOCLIB_BLOCK_OP_WRITE 2
+enum soclib_block_ops_e
+{
+  SOCLIB_BLOCK_OP_NOOP,
+  SOCLIB_BLOCK_OP_READ,
+  SOCLIB_BLOCK_OP_WRITE,
+  SOCLIB_BLOCK_OP_PARTIAL_READ,
+  SOCLIB_BLOCK_OP_PARTIAL_WRITE,
+  SOCLIB_BLOCK_OP_ERASE,
+};
 
-#define SOCLIB_BLOCK_STATUS_IDLE 0
-#define SOCLIB_BLOCK_STATUS_BUSY 1
-#define SOCLIB_BLOCK_STATUS_READ_SUCCESS 2
-#define SOCLIB_BLOCK_STATUS_WRITE_SUCCESS 3
-#define SOCLIB_BLOCK_STATUS_READ_ERROR 4
-#define SOCLIB_BLOCK_STATUS_WRITE_ERROR 5
-#define SOCLIB_BLOCK_STATUS_ERROR 6
+enum soclib_block_status_e
+{
+  SOCLIB_BLOCK_STATUS_IDLE,
+  SOCLIB_BLOCK_STATUS_BUSY,
+  SOCLIB_BLOCK_STATUS_READ_SUCCESS,
+  SOCLIB_BLOCK_STATUS_WRITE_SUCCESS,
+  SOCLIB_BLOCK_STATUS_READ_ERROR,
+  SOCLIB_BLOCK_STATUS_WRITE_ERROR,
+  SOCLIB_BLOCK_STATUS_ERROR,
+  SOCLIB_BLOCK_STATUS_ERASE_SUCCESS,
+  SOCLIB_BLOCK_STATUS_ERASE_ERROR,
+};
+
+enum soclib_block_type_e
+{
+  SOCLIB_BLOCK_TYPE_DISK,
+  SOCLIB_BLOCK_TYPE_RODISK,
+  SOCLIB_BLOCK_TYPE_NOR_FLASH,
+};
+
+
+struct soclib_block_context_s
+{
+  uintptr_t                 addr;
+  struct dev_irq_ep_s       irq_ep;
+  dev_mem_queue_root_t      queue;
+
+  uint_fast8_t              blk_log2;
+  uint32_t                  blk_count;
+  enum soclib_block_type_e  memtype;
+
+  void                      *data;
+  size_t                    size;
+
+  size_t                    progress;
+  bool_t                    running;
+  enum dev_mem_rq_type_e    rqtype;
+};
 
 /**************************************************************/
 
 static void soclib_block_op_start(struct soclib_block_context_s *pv,
-				  struct dev_block_rq_s *rq)
+				  struct dev_mem_rq_s *rq)
 {
-  //printk("New op: %d %d\n", rq->lba, rq->type);
-
-  cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_BUFFER,
-                   endian_le32((uint32_t)rq->data[rq->progress]));
-  cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_LBA,
-                   endian_le32(rq->lba + rq->progress));
-  cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_COUNT,
-                   endian_le32(1));
-
-  switch (rq->type & DEV_BLOCK_OPMASK)
+  if (pv->rqtype & DEV_MEM_OP_PAGE_ERASE)
     {
-    case DEV_BLOCK_READ:
-      cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_OP, endian_le32(SOCLIB_BLOCK_OP_READ));
-      break;
-    case DEV_BLOCK_WRITE:
-      /* invalidate dcache, force dcache write before dma */
-      cpu_dcache_invld_buf(rq->data[rq->progress], pv->params.blk_size);
-      cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_OP, endian_le32(SOCLIB_BLOCK_OP_WRITE));
-      break;
+      cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_LBA,
+                       endian_le32((rq->addr >> pv->blk_log2)));
+      cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_COUNT,
+                       endian_le32(rq->size));
+      cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_OP,
+                       endian_le32(SOCLIB_BLOCK_OP_ERASE));
+    }
+  else if (pv->rqtype & (DEV_MEM_OP_PAGE_READ | DEV_MEM_OP_PAGE_WRITE))
+    { 
+      pv->size = 1 << (pv->blk_log2 + rq->sc_log2);
+      pv->data = rq->sc_data[pv->progress];
+
+      cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_LBA,
+                       endian_le32((rq->addr >> pv->blk_log2) +
+                                   (pv->progress << rq->sc_log2)));
+      cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_BUFFER,
+                       endian_le32((uint32_t)pv->data));
+      cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_COUNT,
+                       endian_le32(1 << rq->sc_log2));
+
+      if (pv->rqtype & DEV_MEM_OP_PAGE_WRITE)
+        {
+          cpu_dcache_invld_buf(pv->data, pv->size);
+          cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_OP,
+                           endian_le32(SOCLIB_BLOCK_OP_WRITE));
+        }
+      else /* if (pv->rqtype & DEV_MEM_OP_PAGE_READ) */
+        {
+          cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_OP,
+                           endian_le32(SOCLIB_BLOCK_OP_READ));
+        }
+    }
+  else if (pv->rqtype & (DEV_MEM_OP_PARTIAL_WRITE | DEV_MEM_OP_PARTIAL_READ))
+    {
+      pv->size = rq->size;
+      pv->data = rq->data;
+
+      cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_LBA,
+                       endian_le32((rq->addr >> pv->blk_log2)));
+      cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_COUNT,
+                       endian_le32(rq->size));
+      cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_BUFFER,
+                       endian_le32((uint32_t)pv->data));
+      cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_OFFSET,
+                       endian_le32((uint32_t)rq->addr & ((1 << pv->blk_log2) - 1)));
+      cpu_dcache_invld_buf(rq->data, rq->size);
+      cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_OP,
+                       endian_le32(pv->rqtype & DEV_MEM_OP_PARTIAL_WRITE
+                                   ? SOCLIB_BLOCK_OP_PARTIAL_WRITE
+                                   : SOCLIB_BLOCK_OP_PARTIAL_READ));
     }
 }
 
+static void soclib_block_rq_end(struct device_s *dev);
+
 /* add a new request in queue and start if idle */
-static void soclib_block_rq_start(struct soclib_block_context_s *pv,
-				  struct dev_block_rq_s *rq)
+static void soclib_block_rq_start(struct device_s *dev)
 {
-  bool_t idle = dev_blk_queue_isempty(&pv->queue);
+  struct soclib_block_context_s *pv = dev->drv_pv;
+  struct dev_mem_rq_s *rq = dev_mem_queue_head(&pv->queue);
 
-  dev_blk_queue_pushback(&pv->queue, rq);
+  if (!pv->running)
+    {
+      pv->running = 1;
+      pv->rqtype = rq->type & (DEV_MEM_OP_PAGE_READ | DEV_MEM_OP_PAGE_WRITE |
+                               DEV_MEM_OP_PARTIAL_READ | DEV_MEM_OP_PARTIAL_WRITE |
+                               DEV_MEM_OP_PAGE_ERASE);
+      rq->err = 0;
 
-  if (idle)
+      if (rq->band_mask & 0xfe)
+        rq->err = -ENOTSUP;
+      else if (rq->type & (DEV_MEM_OP_PAGE_ERASE | DEV_MEM_OP_PAGE_READ | DEV_MEM_OP_PAGE_WRITE))
+        {
+          if (rq->addr & ((1 << pv->blk_log2) - 1))
+            rq->err = -EINVAL;  /* address not page aligned */
+          else if (rq->size & ((1 << rq->sc_log2) - 1))
+            rq->err = -EINVAL;  /* total count not aligned on scattered count */
+          else if ((rq->addr >> pv->blk_log2) + rq->size > pv->blk_count)
+            rq->err = -ERANGE;
+        }
+      else if (rq->type & (DEV_MEM_OP_PARTIAL_WRITE | DEV_MEM_OP_PARTIAL_READ |
+                           DEV_MEM_OP_CACHE_INVALIDATE | DEV_MEM_OP_CACHE_FLUSH))
+        {
+          if (rq->type & (DEV_MEM_OP_PARTIAL_WRITE | DEV_MEM_OP_PARTIAL_READ))
+            {
+              if (rq->addr >> pv->blk_log2 != (rq->addr + rq->size - 1) >> pv->blk_log2)
+                rq->err = -EINVAL;  /* partial access crosses page boundary */
+            }
+          else if (rq->addr + rq->size > (uint64_t)pv->blk_count << pv->blk_log2)
+            rq->err = -ERANGE;
+        }
+      else
+        {
+          rq->err = -ENOTSUP;
+        }
+    }
+
+  pv->progress = 0;
+
+  if (!rq->err && (rq->band_mask & 1) && pv->rqtype && rq->size)
     soclib_block_op_start(pv, rq);
+  else
+    soclib_block_rq_end(dev);
 }
 
 /* drop current request and start next in queue if any */
-static void soclib_block_rq_end(struct soclib_block_context_s *pv)
+static void soclib_block_rq_end(struct device_s *dev)
 {
-  struct dev_block_rq_s *rq;
+  struct soclib_block_context_s *pv = dev->drv_pv;
+  struct dev_mem_rq_s *rq = dev_mem_queue_head(&pv->queue);
 
-  dev_blk_queue_pop(&pv->queue);
+  pv->rqtype = pv->rqtype & (pv->rqtype - 1);
 
-  if ((rq = dev_blk_queue_head(&pv->queue)) != NULL)
-    soclib_block_op_start(pv, rq);
+  if (rq->err || !pv->rqtype)
+    {
+      dev_mem_queue_pop(&pv->queue);
+      lock_release(&dev->lock);
+      kroutine_exec(&rq->kr, 0);
+      lock_spin(&dev->lock);
+      pv->running = 0;
+    }
+
+  if (!dev_mem_queue_isempty(&pv->queue))
+    soclib_block_rq_start(dev);
 }
 
-DEVBLOCK_REQUEST(soclib_block_request)
+static DEVMEM_REQUEST(soclib_block_request)
 {
-  struct device_s *dev = bdev->dev;
+  struct device_s *dev = mdev->dev;
   struct soclib_block_context_s *pv = dev->drv_pv;
-  struct dev_block_params_s *p = &pv->params;
 
   LOCK_SPIN_IRQ(&dev->lock);
 
-  if (rq->lba + rq->count <= p->blk_count)
-    {
-      switch (rq->type & DEV_BLOCK_OPMASK)
-	{
-	case DEV_BLOCK_READ:
-	case DEV_BLOCK_WRITE:
-	  soclib_block_rq_start(pv, rq);
-	  break;
+  dev_mem_queue_pushback(&pv->queue, rq);
 
-	default:
-	  rq->progress = -ENOTSUP;
-	  rq->callback(rq, 0);
-	  break;
-	}
-    }
-  else
-    {
-      rq->progress = -ERANGE;
-      rq->callback(rq, 0);
-    }
+  if (!pv->running)
+    soclib_block_rq_start(dev);
 
   LOCK_RELEASE_IRQ(&dev->lock);
 }
 
-DEVBLOCK_GETPARAMS(soclib_block_getparams)
+static DEVMEM_INFO(soclib_block_info)
 {
-  struct device_s *dev = bdev->dev;
-  struct soclib_block_context_s *pv = dev->drv_pv;
-  return &pv->params;
+  struct device_s *dev = mdev->dev;
+  struct soclib_block_context_s	*pv = dev->drv_pv;
+
+  if (band_index > 0 || mdev->number > 0)
+    return -ENOENT;
+
+  memset(info, 0, sizeof(*info));
+
+  info->size = pv->blk_count;
+  info->page_log2 = pv->blk_log2;
+  switch (pv->memtype)
+    {
+    case SOCLIB_BLOCK_TYPE_DISK:
+      info->flags |= DEV_MEM_WRITABLE;
+    case SOCLIB_BLOCK_TYPE_RODISK:
+      info->type = DEV_MEM_DISK;
+      break;
+    case SOCLIB_BLOCK_TYPE_NOR_FLASH:
+      info->type = DEV_MEM_FLASH;
+      info->erase_log2 = info->page_log2;
+      info->flags |= DEV_MEM_ERASE_ONE | DEV_MEM_WRITABLE |
+        DEV_MEM_PARTIAL_WRITE | DEV_MEM_PARTIAL_READ;
+      break;
+    }
+
+  return 0;
 }
 
 static DEV_IRQ_EP_PROCESS(soclib_block_irq)
@@ -166,39 +286,45 @@ static DEV_IRQ_EP_PROCESS(soclib_block_irq)
 
   lock_spin(&dev->lock);
 
-  struct dev_block_rq_s *rq = dev_blk_queue_head(&pv->queue);
+  struct dev_mem_rq_s *rq = dev_mem_queue_head(&pv->queue);
 
-  uint32_t st = endian_le32(cpu_mem_read_32(pv->addr + SOCLIB_BLOCK_STATUS));
-
-  /* printk("block dev irq %p, st: %x\n", dev, st); */
+  enum soclib_block_status_e st = endian_le32(cpu_mem_read_32(pv->addr + SOCLIB_BLOCK_STATUS));
 
   switch (st)
     {
     case SOCLIB_BLOCK_STATUS_READ_SUCCESS:
-      soclib_mem_mark_initialized(rq->data[rq->progress], pv->params.blk_size);
       /* invalidate dcache after dma */
-      cpu_dcache_invld_buf(rq->data[rq->progress], pv->params.blk_size);
+      cpu_dcache_invld_buf(pv->data, pv->size);
+      soclib_mem_mark_initialized(pv->data, pv->size);
 
+    case SOCLIB_BLOCK_STATUS_ERASE_SUCCESS:
     case SOCLIB_BLOCK_STATUS_WRITE_SUCCESS:
       assert(rq != NULL);
 
-      rq->progress++;
-      rq->callback(rq, 1);
+      if (rq->type & (DEV_MEM_OP_PAGE_READ | DEV_MEM_OP_PAGE_WRITE))
+        {
+          if ((++pv->progress << rq->sc_log2) < rq->size)
+            {
+              soclib_block_op_start(pv, rq);
+              break;
+            }
+        }
 
-      if (rq->progress < rq->count)
-        soclib_block_op_start(pv, rq);
-      else
-        soclib_block_rq_end(pv);
+      soclib_block_rq_end(dev);
       break;
 
     case SOCLIB_BLOCK_STATUS_ERROR:
     case SOCLIB_BLOCK_STATUS_READ_ERROR:
     case SOCLIB_BLOCK_STATUS_WRITE_ERROR:
+    case SOCLIB_BLOCK_STATUS_ERASE_ERROR:
       assert(rq != NULL);
 
-      rq->progress = -EIO;
-      rq->callback(rq, 0);
-      soclib_block_rq_end(pv);
+      rq->err = -EIO;
+      soclib_block_rq_end(dev);
+      break;
+
+    case SOCLIB_BLOCK_STATUS_IDLE:
+    case SOCLIB_BLOCK_STATUS_BUSY:
       break;
     }
 
@@ -207,15 +333,15 @@ static DEV_IRQ_EP_PROCESS(soclib_block_irq)
 
 static const struct devenum_ident_s	soclib_block_ids[] =
 {
-	DEVENUM_FDTNAME_ENTRY("soclib:vci_block_device"),
-	{ 0 }
+  DEVENUM_FDTNAME_ENTRY("soclib:vci_block_device"),
+  { 0 }
 };
 
-static const struct driver_block_s	soclib_block_block_drv =
+static const struct driver_mem_s	soclib_block_mem_drv =
 {
-  .class_		= DRIVER_CLASS_BLOCK,
+  .class_		= DRIVER_CLASS_MEM,
+  .f_info               = soclib_block_info,
   .f_request		= soclib_block_request,
-  .f_getparams          = soclib_block_getparams,
 };
 
 static DEV_INIT(soclib_block_init);
@@ -227,7 +353,7 @@ const struct driver_s	soclib_block_drv =
   .id_table		= soclib_block_ids,
   .f_init		= soclib_block_init,
   .f_cleanup		= soclib_block_cleanup,
-  .classes              = { &soclib_block_block_drv, 0 }
+  .classes              = { &soclib_block_mem_drv, 0 }
 };
 
 REGISTER_DRIVER(soclib_block_drv);
@@ -251,21 +377,19 @@ static DEV_INIT(soclib_block_init)
   device_irq_source_init(dev, &pv->irq_ep, 1,
                          &soclib_block_irq, DEV_IRQ_SENSE_HIGH_LEVEL);
 
-  dev_blk_queue_init(&pv->queue);
+  dev_mem_queue_init(&pv->queue);
 
   if (device_irq_source_link(dev, &pv->irq_ep, 1, 1))
     goto err_q;
 
-  pv->params.blk_size = endian_le32(
-      cpu_mem_read_32(pv->addr + SOCLIB_BLOCK_BLOCK_SIZE));
-  pv->params.blk_count = endian_le32(
-      cpu_mem_read_32(pv->addr + SOCLIB_BLOCK_SIZE));
+  pv->blk_count = endian_le32(cpu_mem_read_32(pv->addr + SOCLIB_BLOCK_SIZE));
+  pv->blk_log2 = ffs(endian_le32(cpu_mem_read_32(pv->addr + SOCLIB_BLOCK_BLOCK_SIZE))) - 1;
+  pv->memtype = endian_le32(cpu_mem_read_32(pv->addr + SOCLIB_BLOCK_TYPE));
+
+  pv->running = 0;
 
   cpu_mem_write_32(pv->addr + SOCLIB_BLOCK_IRQ_ENABLE,
                    endian_le32(1));
-
-  printk("Soclib block device : %u sectors %u bytes per block\n",
-	 pv->params.blk_count, pv->params.blk_size);
 
   dev->drv = &soclib_block_drv;
   dev->status = DEVICE_DRIVER_INIT_DONE;
@@ -273,7 +397,7 @@ static DEV_INIT(soclib_block_init)
   return 0;
 
  err_q:
-  dev_blk_queue_destroy(&pv->queue);
+  dev_mem_queue_destroy(&pv->queue);
  err_mem:
   mem_free(pv);
   return -1;
@@ -287,7 +411,7 @@ static DEV_CLEANUP(soclib_block_cleanup)
 
   device_irq_source_unlink(dev, &pv->irq_ep, 1);
 
-  dev_blk_queue_destroy(&pv->queue);
+  dev_mem_queue_destroy(&pv->queue);
 
   mem_free(pv);
 }
