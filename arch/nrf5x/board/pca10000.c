a9ed80dacd175fa7bfffb0ed3a912b31c7144e78
Nicolas Pouillon <nipo@ssji.net>
2014-10-13 19:14:19 +0200

    arch/nrf5x: Add support for PCA10000 (nRF51822-Dongle)

---
+++ arch/nrf5x/board/pca10000.c
@@ -0,0 +1,44 @@
+/*
+    This file is part of MutekH.
+
+    MutekH is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; version 2.1 of the
+    License.
+
+    MutekH is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program.  If not, see
+    <http://www.gnu.org/licenses/>.
+
+    Copyright Nicolas Pouillon <nipo@ssji.net> (c) 2014
+*/
+
+#include <arch/nrf5x/ids.h>
+
+#ifdef CONFIG_DEVICE
+# include <device/class/iomux.h>
+# include <device/class/uart.h>
+# include <device/resources.h>
+# include <device/irq.h>
+#endif
+
+#ifdef CONFIG_DRIVER_NRF5X_UART
+
+DEV_DECLARE_STATIC(uart_dev, "uart0", 0, nrf5x_uart_drv,
+                   NRF_STATIC_RES_PERIPHERAL_MEM(NRF5X_UART0),
+                   DEV_STATIC_RES_DEV_ICU("/cpu"),
+                   DEV_STATIC_RES_IRQ(0, NRF5X_UART0, DEV_IRQ_SENSE_HIGH_LEVEL, 0, 1),
+                   DEV_STATIC_RES_DEV_IOMUX("/gpio"),
+                   DEV_STATIC_RES_UART(1000000, 8, DEV_UART_PARITY_NONE, 1, 1, 0),
+                   DEV_STATIC_RES_IOMUX("rts", 0, 8, 0, 0),
+                   DEV_STATIC_RES_IOMUX("tx", 0, 9, 0, 0),
+                   DEV_STATIC_RES_IOMUX("cts", 0, 10, 0, 0),
+                   DEV_STATIC_RES_IOMUX("rx", 0, 11, 0, 0)
+                   );
+
+#endif
