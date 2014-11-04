036edeccec931e9c4450b3b85b2b36ffa19211a1
Alexandre Becoulet <alexandre.becoulet@free.fr>
2014-11-04 11:43:48 +0100

    soclib: updated the tty char driver

--- arch/soclib/drivers/vci_multi_tty/tty.c
+++ arch/soclib/drivers/vci_multi_tty/tty.c
@@ -40,13 +40,13 @@
 #define GCT_CONTAINER_ALGO_tty_fifo RING
 GCT_CONTAINER_TYPES(tty_fifo, uint8_t, 256);
 GCT_CONTAINER_FCNS(tty_fifo, static inline, tty_fifo,
-                   init, destroy, pop_array, pushback);
+                   init, destroy, pop_array, pushback, isfull);
 #endif
 
 struct tty_soclib_context_s
 {
   /* tty input request queue and char fifo */
-  dev_char_queue_root_t		read_q;
+  dev_request_queue_root_t	read_q;
 #ifdef CONFIG_DEVICE_IRQ
   tty_fifo_root_t		read_fifo;
   struct dev_irq_ep_s           irq_ep;
@@ -61,10 +61,11 @@ struct tty_soclib_context_s
 void tty_soclib_try_read(struct device_s *dev)
 {
   struct tty_soclib_context_s	*pv = dev->drv_pv;
-  struct dev_char_rq_s		*rq;
 
-  while ((rq = dev_char_queue_head(&pv->read_q)))
+  while (!dev_request_queue_isempty(&pv->read_q))
   {
+    struct dev_char_rq_s *rq =
+      dev_char_rq_s_cast(dev_request_queue_head(&pv->read_q));
     size_t size;
 
 #ifdef CONFIG_DEVICE_IRQ
@@ -84,8 +85,9 @@ void tty_soclib_try_read(struct device_s *dev)
     rq->data += size;
 
     if (rq->size == 0 || rq->type == DEV_CHAR_READ_NONBLOCK) {
+      dev_request_queue_pop(&pv->read_q);
       lock_release(&dev->lock);
-      kroutine_exec(&rq->kr, cpu_is_interruptible());
+      kroutine_exec(&rq->base.kr, cpu_is_interruptible());
       lock_spin(&dev->lock);
     }
   }
@@ -95,25 +97,28 @@ DEVCHAR_REQUEST(tty_soclib_request)
 {
   struct device_s               *dev = cdev->dev;
   struct tty_soclib_context_s	*pv = dev->drv_pv;
-  struct dev_char_rq_s *done_rq = NULL;
+  struct dev_char_rq_s          *done_rq = NULL;
 
   assert(rq->size);
   assert(cdev->number == 0);
 
   LOCK_SPIN_IRQ(&dev->lock);
 
+  bool_t empty = dev_request_queue_isempty(&pv->read_q);
+
   switch (rq->type)
   {
   case DEV_CHAR_READ_NONBLOCK:
-  case DEV_CHAR_READ: {
-    bool_t empty;
-
-    empty = dev_char_queue_isempty(&pv->read_q);
-    dev_char_queue_pushback(&pv->read_q, rq);
+    if (!empty)
+      {
+        done_rq = rq;
+        break;
+      }
+  case DEV_CHAR_READ:
+    dev_request_queue_pushback(&pv->read_q, dev_char_rq_s_base(rq));
     if (empty)
       tty_soclib_try_read(dev);
     break;
-  }
 
   case DEV_CHAR_WRITE_NONBLOCK:
   case DEV_CHAR_WRITE: {
@@ -132,7 +137,7 @@ DEVCHAR_REQUEST(tty_soclib_request)
   LOCK_RELEASE_IRQ(&dev->lock);
 
   if (done_rq)
-    kroutine_exec(&done_rq->kr, cpu_is_interruptible());
+    kroutine_exec(&done_rq->base.kr, cpu_is_interruptible());
 }
 
 #ifdef CONFIG_DEVICE_IRQ
@@ -146,15 +151,20 @@ static DEV_IRQ_EP_PROCESS(tty_soclib_irq)
   lock_spin(&dev->lock);
 
   while ( cpu_mem_read_8(pv->addr + TTY_SOCLIB_REG_STATUS) ) {
-	  /* get character from tty */
-	  c = cpu_mem_read_8(pv->addr + TTY_SOCLIB_REG_READ);
 
-	  /* add character to driver fifo, discard if fifo full */
-	  tty_fifo_pushback(&pv->read_fifo, c);
-          *id = 0;
+    do {
+      /* get character from tty */
+      c = cpu_mem_read_8(pv->addr + TTY_SOCLIB_REG_READ);
+
+      /* add character to driver fifo, discard if fifo full */
+      tty_fifo_pushback(&pv->read_fifo, c);
+      *id = 0;
+    } while ( cpu_mem_read_8(pv->addr + TTY_SOCLIB_REG_STATUS)
+              && ! tty_fifo_isfull(&pv->read_fifo));
+
+    tty_soclib_try_read(dev);
   }
 
-  tty_soclib_try_read(dev);
   lock_release(&dev->lock);
 }
 
@@ -201,7 +211,7 @@ DEV_INIT(tty_soclib_init)
   if (!pv)
     return -ENOMEM;
 
-  dev_char_queue_init(&pv->read_q);
+  dev_request_queue_init(&pv->read_q);
 
   if (device_res_get_uint(dev, DEV_RES_MEM, 0, &pv->addr, NULL))
     goto err_mem;
@@ -241,7 +251,7 @@ DEV_CLEANUP(tty_soclib_cleanup)
   tty_fifo_destroy(&pv->read_fifo);
 #endif
 
-  dev_char_queue_destroy(&pv->read_q);
+  dev_request_queue_destroy(&pv->read_q);
 
   mem_free(pv);
 }
