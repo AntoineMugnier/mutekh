9c4924d1324c90ecd443788681805e1fc7e96dcd
Nicolas Pouillon <nipo@ssji.net>
2015-01-27 16:33:14 +0100

    arch/nrf5x: Add support for PCA10005 (nRF51-DK daughterboard with SMA)

---
+++ arch/nrf5x/board/pca10005.c
@@ -0,0 +1,42 @@
+/*
+    This file is part of MutekH.
+
+    MutekH is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; version 2.1 of the
+    License.
+
+    MutekH is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program.  If not, see
+    <http://www.gnu.org/licenses/>.
+
+    Copyright Nicolas Pouillon <nipo@ssji.net> (c) 2014
+*/
+
+#ifdef CONFIG_DEVICE
+#include <device/resources.h>
+#include <device/class/iomux.h>
+#include <device/class/uart.h>
+#include <arch/nrf5x/ids.h>
+#endif
+
+#if defined(CONFIG_DRIVER_NRF5X_UART)
+
+DEV_DECLARE_STATIC(uart_dev, "uart0", 0, nrf5x_uart_drv,
+                   NRF_STATIC_RES_PERIPHERAL_MEM(NRF5X_UART0),
+                   DEV_STATIC_RES_DEV_PARAM("icu", "/cpu"),,
+                   DEV_STATIC_RES_IRQ(0, NRF5X_UART0, DEV_IRQ_SENSE_HIGH_LEVEL, 0, 1)
+                   DEV_STATIC_RES_DEV_PARAM("iomux", "/gpio"),
+                   DEV_STATIC_RES_UART(1000000, 8, DEV_UART_PARITY_NONE, 1, 1, 0),
+                   DEV_STATIC_RES_IOMUX("rts", 0, 0, 0, 0),
+                   DEV_STATIC_RES_IOMUX("tx", 0, 1, 0, 0),
+                   DEV_STATIC_RES_IOMUX("cts", 0, 3, 0, 0),
+                   DEV_STATIC_RES_IOMUX("rx", 0, 2, 0, 0)
+                   );
+
+#endif
