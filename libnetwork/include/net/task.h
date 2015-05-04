f6f016e23763ef4c22f3aca229ee2169b55ff07d
Nicolas Pouillon <nipo@ssji.net>
2015-05-04 08:44:28 +0200

    libnetwork: Add license, remove broken code

--- libnetwork/include/net/task.h
+++ libnetwork/include/net/task.h
@@ -1,3 +1,23 @@
+/*
+    This file is part of MutekH.
+
+    MutekH is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; version 2.1 of the
+    License.
+
+    MutekH is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program.  If not, see
+    <http://www.gnu.org/licenses/>.
+
+    Copyright Nicolas Pouillon <nipo@ssji.net> (c) 2015
+*/
+
 #ifndef NET_TASK_H
 #define NET_TASK_H
 
@@ -22,7 +42,6 @@
 struct net_scheduler_s;
 struct net_task_s;
 struct net_task_header_s;
-struct net_stack_s;
 struct net_layer_s;
 
 void net_scheduler_task_push(
