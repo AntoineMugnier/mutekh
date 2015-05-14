a2d981231e5849f76dc97cab24a859174a211831
Nicolas Pouillon <nipo@ssji.net>
2015-05-14 14:25:00 +0200

    arch/psoc4: Add base IO files, printk support

---
+++ arch/psoc4/include/psoc4/memory_map.h
@@ -0,0 +1,27 @@
+/*
+    This file is part of MutekH.
+
+    MutekH is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; version 2.1 of the
+    License.
+
+    MutekH is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program.  If not, see
+    <http://www.gnu.org/licenses/>.
+
+    Copyright (c) Nicolas Pouillon <nipo@ssji.net>, 2015
+*/
+
+#ifndef PSOC4_MEMORY_MAP_H
+#define PSOC4_MEMORY_MAP_H
+
+#define PSOC4_SCB0_ADDR 0x40240000
+#define PSOC4_SCB1_ADDR 0x40250000
+
+#endif
