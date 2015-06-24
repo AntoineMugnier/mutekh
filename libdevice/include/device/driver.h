78ab09ac9e13fc74fa2ac50cd7165d47f7a42141
Nicolas Pouillon <nipo@ssji.net>
2015-06-24 17:18:00 +0200

    libdevice: Add ble_radio class

--- libdevice/include/device/driver.h
+++ libdevice/include/device/driver.h
@@ -63,6 +63,7 @@ enum driver_class_e
   DRIVER_CLASS_CPU,
   DRIVER_CLASS_VALIO,
   DRIVER_CLASS_PERSIST,
+  DRIVER_CLASS_BLE_RADIO,
 
   /** Custom driver class IDs should be registered in @ref
       #CONFIG_DEVICE_CUSTOM_CLASS_COUNT enum config token.
