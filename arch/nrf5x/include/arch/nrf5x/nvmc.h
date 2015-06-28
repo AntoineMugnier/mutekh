ca47c4f52b6c2a3eb35e617b672a22cae7a50245
Nicolas Pouillon <nipo@ssji.net>
2015-06-29 00:49:22 +0200

    arch/nrf5x: Add NVMC definitions

---
+++ arch/nrf5x/include/arch/nrf5x/nvmc.h
@@ -0,0 +1,48 @@
+/*
+    This file is part of MutekH.
+
+    MutekH is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; version 2.1 of the
+    License.
+
+    MutekH is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program.  If not, see
+    <http://www.gnu.org/licenses/>.
+
+    Copyright Nicolas Pouillon <nipo@ssji.net> (c) 2014
+*/
+
+#ifndef ARCH_NRF_NVMC_H_
+#define ARCH_NRF_NVMC_H_
+
+#include "peripheral.h"
+#include "ids.h"
+
+enum nrf5x_nvmc_register {
+    NRF_NVMC_READY = 0,
+    NRF_NVMC_CONFIG = 65,
+    NRF_NVMC_ERASEPAGE = 66,
+    NRF_NVMC_PCR1 = 66,
+    NRF_NVMC_ERASEALL = 67,
+    NRF_NVMC_ERASEPCR0 = 68,
+    NRF_NVMC_ERASEUICR = 69,
+    NRF_NVMC_ICACHECNF = 80,
+    NRF_NVMC_IHIT = 82,
+    NRF_NVMC_IMISS = 83,
+};
+
+#define NRF_NVMC_CONFIG_WRITE (1 << 0)
+#define NRF_NVMC_CONFIG_ERASE (1 << 1)
+
+#define NRF_NVMC_ICACHECNF_CACHEEN_ENABLED (1 << 0)
+#define NRF_NVMC_ICACHECNF_CACHEEN_DISABLED (0 << 0)
+#define NRF_NVMC_ICACHECNF_CACHEPROFEN_ENABLED (1 << 8)
+#define NRF_NVMC_ICACHECNF_CACHEPROFEN_DISABLED (0 << 8)
+
+#endif
