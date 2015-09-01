dc2a95e3d20c1edfc4754daf941c7bfd45f839b5
Nicolas Pouillon <nipo@ssji.net>
2015-09-01 14:50:14 +0200

    arch/nrf5x: Add support for PCA10028 (nRF51-DK)

---
+++ arch/nrf5x/board/pca10028.c
@@ -0,0 +1,53 @@
+/*
+    This file is part of MutekH.
+
+    MutekH is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; version 2.1 of the
+    License.
+
+    MutekH is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program.  If not, see
+    <http://www.gnu.org/licenses/>.
+
+    Copyright Nicolas Pouillon <nipo@ssji.net> (c) 2015
+*/
+
+#include <device/resources.h>
+#include <device/class/iomux.h>
+#include <device/class/uart.h>
+#include <device/class/gpio.h>
+#include <arch/nrf5x/ids.h>
+
+#ifdef CONFIG_DRIVER_NRF5X_UART
+
+DEV_DECLARE_STATIC(uart_dev, "uart0", 0, nrf5x_uart_drv,
+                   NRF_STATIC_RES_PERIPHERAL_MEM(NRF5X_UART0),
+                   DEV_STATIC_RES_DEV_PARAM("icu", "/cpu"),
+                   DEV_STATIC_RES_IRQ(0, NRF5X_UART0, DEV_IRQ_SENSE_HIGH_LEVEL, 0, 1),
+                   DEV_STATIC_RES_DEV_PARAM("iomux", "/gpio"),
+                   DEV_STATIC_RES_UART(1000000, 8, DEV_UART_PARITY_NONE, 1, 1, 0),
+                   DEV_STATIC_RES_IOMUX("rts", 0, 8, 0, 0),
+                   DEV_STATIC_RES_IOMUX("tx", 0, 9, 0, 0),
+                   DEV_STATIC_RES_IOMUX("cts", 0, 10, 0, 0),
+                   DEV_STATIC_RES_IOMUX("rx", 0, 11, 0, 0)
+                   );
+
+#endif
+
+#if defined(CONFIG_DRIVER_NRF5X_GPIO) && defined(CONFIG_DRIVER_BUTTON_SET)
+
+DEV_DECLARE_STATIC(keyboard_dev, "keyboard", 0, button_set_drv,
+                   DEV_STATIC_RES_DEV_PARAM("icu", "/gpio"),
+                   DEV_STATIC_RES_IRQ(0, NRF_GPIO_RANGE_IRQ_ID, DEV_IRQ_SENSE_HIGH_LEVEL, 0, 1),
+                   DEV_STATIC_RES_IO(17, 20),
+                   DEV_STATIC_RES_BLOB_PARAM("mask", dev_gpio_mask1),
+                   DEV_STATIC_RES_UINT_PARAM("active", 0),
+                   );
+
+#endif
