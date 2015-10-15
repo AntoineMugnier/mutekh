ac3b8ea3ce0de2509bf995bd13245fb890b26f71
Julien Peeters <contact@julienpeeters.net>
2015-10-15 10:49:28 +0200

    zynq: fix missing power_* implementations

---
+++ arch/zynq/power.c
@@ -0,0 +1,38 @@
+/*
+    This file is part of MutekH.
+
+    MutekH is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; version 2.1 of the
+    License.
+
+    MutekH is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with MutekH; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301 USA.
+
+    Copyright Julien Peeters <contact@julienpeeters.net> (c) 2014-2015
+
+*/
+
+#include <hexo/power.h>
+
+error_t power_reboot()
+{
+  return ENOTSUP;
+}
+
+error_t power_shutdown()
+{
+  return ENOTSUP;
+}
+
+enum power_reset_cause_e power_reset_cauase(void)
+{
+  return POWER_RESET_CAUSE_UNKNOWN;
+}
