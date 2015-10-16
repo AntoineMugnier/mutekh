1088f5522b74d8f6dca11aef729ba8a635f6a8a0
Nicolas Pouillon <nipo@ssji.net>
2015-10-16 09:42:54 +0200

    examples/ble: Add BLE RGB Led example

---
+++ examples/ble/rgb_led/led.h
@@ -0,0 +1,21 @@
+#ifndef LED_H_
+#define LED_H_
+
+#include <ble/gattdb/db.h>
+#include <ble/gattdb/service.h>
+#include <device/class/pwm.h>
+
+struct led_s
+{
+  struct ble_gattdb_registry_s dbs;
+  struct device_pwm_s pwm;
+
+  struct dev_pwm_config_s cfg[3];
+};
+
+STRUCT_COMPOSE(led_s, dbs);
+
+error_t led_service_register(struct led_s *led,
+                             struct ble_gattdb_s *db);
+
+#endif
