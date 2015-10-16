8bfddc8242ce1a647af0ac3180ae3e079abd7b6a
Nicolas Pouillon <nipo@ssji.net>
2015-10-16 09:42:54 +0200

    examples/ble: Add BLE RGB Led example

---
+++ examples/ble/rgb_led/dis.c
@@ -0,0 +1,24 @@
+#include <ble/gattdb/db.h>
+#include <ble/gattdb/service.h>
+#include <ble/gatt/characteristic.h>
+#include <ble/gatt/service.h>
+
+#include "dis.h"
+
+BLE_GATTDB_SERVICE_DECL(dis_service,
+                      BLE_GATTDB_SERVICE_PRIMARY,
+                      BLE_UUID_BT_BASED_P(BLE_GATT_SERVICE_DEVICE_INFORMATION),
+                      NULL,
+                      BLE_GATTDB_CHAR_CONSTANT_STRING(
+                          BLE_UUID_BT_BASED_P(BLE_GATT_CHAR_MANUFACTURER_NAME_STRING),
+                          "MutekH"),
+                      BLE_GATTDB_CHAR_CONSTANT_STRING(
+                          BLE_UUID_BT_BASED_P(BLE_GATT_CHAR_MODEL_NUMBER_STRING),
+                          "BLE RGB Led"),
+                      );
+
+error_t dis_service_register(struct ble_gattdb_registry_s *dbs,
+                             struct ble_gattdb_s *db)
+{
+    return ble_gattdb_service_register(dbs, db, &dis_service);
+}
