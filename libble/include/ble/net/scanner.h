b143cebdf3b3ac93830c0c27a455cd3369ee182a
Nicolas Pouillon <nipo@ssji.net>
2015-10-31 08:20:21 +0100

    libble: Update scanner decl

--- libble/include/ble/net/scanner.h
+++ libble/include/ble/net/scanner.h
@@ -34,8 +34,8 @@ struct dev_rng_s;
 
 struct ble_scanner_param_s
 {
-  dev_timer_delay_t interval;
-  dev_timer_delay_t duration;
+  uint32_t interval_ms;
+  uint32_t duration_ms;
   struct ble_addr_s local_addr;
 };
 
@@ -50,15 +50,50 @@ struct ble_scanner_device_s
   bool_t connectable;
 };
 
+enum ble_scanner_device_state_e
+{
+  BLE_SCANNER_ADV_SEEN,
+  BLE_SCANNER_SCANNED,
+  BLE_SCANNER_LOST,
+};
+
+enum ble_scanner_device_action_e
+{
+  BLE_SCANNER_IGNORE,
+  BLE_SCANNER_SCAN,
+  BLE_SCANNER_CONNECT,
+};
+
+struct ble_scanner_handler_s
+{
+  struct net_layer_handler_s base;
+
+  error_t (*params_update)(struct net_layer_s *layer, const struct ble_scanner_param_s *params);
+};
+
+STRUCT_COMPOSE(ble_advertiser_handler_s, base);
+
 struct ble_scanner_delegate_vtable_s
 {
   struct net_layer_delegate_vtable_s base;
 
-  void (*device_added)(void *delegate, struct net_layer_s *layer, struct ble_scanner_device_s *dev);
-  void (*device_updated)(void *delegate, struct net_layer_s *layer, struct ble_scanner_device_s *dev);
-  void (*device_removed)(void *delegate, struct net_layer_s *layer, struct ble_scanner_device_s *dev);
+  enum ble_scanner_device_action_e (*device_action)(void *delegate, struct net_layer_s *layer, const struct ble_scanner_device_s *dev);
+
+  bool_t (*connection_requested)(void *delegate, struct net_layer_s *layer,
+                                 const struct ble_adv_connect_s *conn,
+                                 dev_timer_value_t anchor);
 };
 
 STRUCT_COMPOSE(ble_scanner_delegate_vtable_s, base);
+
+ALWAYS_INLINE
+error_t ble_scanner_params_update(struct net_layer_s *layer,
+                                  const struct ble_scanner_param_s *params)
+{
+  const struct ble_scanner_handler_s *handler
+    = const_ble_scanner_handler_s_from_base(layer->handler);
+
+  return handler->params_update(layer, params);
+}
 
 #endif
