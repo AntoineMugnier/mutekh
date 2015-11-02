59614ab9af301d9b241c8fc057d48392d06a8358
Nicolas Pouillon <nipo@ssji.net>
2015-11-02 01:45:07 +0100

    libble: Add LLCP layer definition

---
+++ libble/include/ble/net/llcp.h
@@ -0,0 +1,105 @@
+/*
+    This file is part of MutekH.
+
+    MutekH is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; version 2.1 of the
+    License.
+
+    MutekH is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program.  If not, see
+    <http://www.gnu.org/licenses/>.
+
+    Copyright (c) Nicolas Pouillon <nipo@ssji.net> 2015
+*/
+
+#ifndef BLE_LLCP_H_
+#define BLE_LLCP_H_
+
+/**
+   @file
+   @module{BLE library}
+   @short Link-Layer Control Protocol network layer
+
+   @section {Description}
+
+   This implements LE-C channel handling of a connection.
+
+   @end section
+*/
+
+#include <hexo/types.h>
+#include <hexo/decls.h>
+#include <net/task.h>
+#include <ble/uuid.h>
+#include <ble/protocol/att.h>
+
+struct net_layer_s;
+struct net_scheduler_s;
+
+enum ble_llcp_task_id_e
+{
+  BLE_LLCP_CONNECTION_PARAMETERS_UPDATE = 0xllc20000,
+  BLE_LLCP_CHANNEL_MAP_UPDATE,
+  BLE_LLCP_CONNECTION_PARAMETERS_REQ,
+  BLE_LLCP_ENCRYPTION_SETUP,
+  BLE_LLCP_ENCRYPTION_ENABLE,
+  BLE_LLCP_LENGTH_UPDATE,
+  BLE_LLCP_PING,
+};
+
+struct ble_llcp_connection_parameters_update_s
+{
+  struct net_task_s task;
+
+  struct ble_conn_timing_param_s timing;
+  uint16_t win_offset;
+  uint16_t instant;
+};
+
+STRUCT_COMPOSE(ble_llcp_connection_parameters_update_s, task);
+
+struct ble_llcp_channel_map_update_s
+{
+  struct net_task_s task;
+
+  uint64_t channel_map;
+  uint16_t instant;
+};
+
+STRUCT_COMPOSE(ble_llcp_channel_map_update_s, task);
+
+struct ble_llcp_encryption_setup_s
+{
+  struct net_task_s task;
+
+  uint8_t sk[16];
+  uint8_t iv[8];
+};
+
+STRUCT_COMPOSE(ble_llcp_encryption_setup_s, task);
+
+struct ble_llcp_encryption_enable_s
+{
+  struct net_task_s task;
+
+  bool_t enabled;
+};
+
+STRUCT_COMPOSE(ble_llcp_encryption_enable_s, task);
+
+struct ble_llcp_length_update_s
+{
+  struct net_task_s task;
+
+  uint16_t rx_mtu;
+};
+
+STRUCT_COMPOSE(ble_llcp_encryption_enable_s, task);
+
+#endif
