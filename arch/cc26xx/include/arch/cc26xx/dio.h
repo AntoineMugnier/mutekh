ed7b5b3f3a64bd30e5428ce1fc4afde6b5a0f276
Vincent Defilippi <vincentdefilippi@gmail.com>
2015-11-10 11:13:18 +0100

    cc26xx: add forgot headers

---
+++ arch/cc26xx/include/arch/cc26xx/dio.h
@@ -0,0 +1,37 @@
+#ifndef CC26XX_DIO_H
+#define CC26XX_DIO_H
+
+#define CC26XX_DIO0    0
+#define CC26XX_DIO1    1
+#define CC26XX_DIO2    2
+#define CC26XX_DIO3    3
+#define CC26XX_DIO4    4
+#define CC26XX_DIO5    5
+#define CC26XX_DIO6    6
+#define CC26XX_DIO7    7
+#define CC26XX_DIO8    8
+#define CC26XX_DIO9    9
+#define CC26XX_DIO10  10
+#define CC26XX_DIO11  11
+#define CC26XX_DIO12  12
+#define CC26XX_DIO13  13
+#define CC26XX_DIO14  14
+#define CC26XX_DIO15  15
+#define CC26XX_DIO16  16
+#define CC26XX_DIO17  17
+#define CC26XX_DIO18  18
+#define CC26XX_DIO19  19
+#define CC26XX_DIO20  20
+#define CC26XX_DIO21  21
+#define CC26XX_DIO22  22
+#define CC26XX_DIO23  23
+#define CC26XX_DIO24  24
+#define CC26XX_DIO25  25
+#define CC26XX_DIO26  26
+#define CC26XX_DIO27  27
+#define CC26XX_DIO28  28
+#define CC26XX_DIO29  29
+#define CC26XX_DIO30  30
+#define CC26XX_DIO31  31
+
+#endif
