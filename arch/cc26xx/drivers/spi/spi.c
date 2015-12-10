4e34d200c3968e99fea3e670ecdc4a925f204abc
Vincent Defilippi <vincentdefilippi@gmail.com>
2015-12-10 17:34:27 +0100

    cc26xx: add spi driver

---
+++ arch/cc26xx/drivers/spi/spi.c
@@ -0,0 +1,440 @@
+/*
+    This file is part of MutekH.
+    
+    MutekH is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; version 2.1 of the
+    License.
+    
+    MutekH is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+    
+    You should have received a copy of the GNU Lesser General Public
+    License along with MutekH; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301 USA.
+
+    Copyright Vincent DEFILIPPI <vincentdefilippi@gmail.com> (c) 2015
+*/
+
+#include <hexo/types.h>
+#include <hexo/endian.h>
+#include <hexo/iospace.h>
+#include <hexo/interrupt.h>
+
+#include <mutek/mem_alloc.h>
+#include <mutek/printk.h>
+
+#include <device/device.h>
+#include <device/resources.h>
+#include <device/driver.h>
+#include <device/irq.h>
+#include <device/class/spi.h>
+#include <device/class/timer.h>
+#include <device/class/iomux.h>
+#include <device/class/clock.h>
+
+#include <arch/cc26xx/memory_map.h>
+#include <arch/cc26xx/prcm.h>
+#include <arch/cc26xx/ssi.h>
+
+#define CC26XX_SPI_FIFO_SIZE 8
+
+struct cc26xx_spi_context_s
+{
+  uintptr_t                      addr;
+#ifdef CONFIG_DEVICE_IRQ
+  struct dev_irq_src_s           irq_ep;
+#endif
+  struct dev_spi_ctrl_transfer_s *tr;
+  uint_fast8_t                   fifo_lvl;
+
+#ifdef CONFIG_DEVICE_SPI_REQUEST
+  struct dev_spi_ctrl_queue_s    queue;
+#endif
+
+  struct dev_freq_s              freq;
+  uint32_t                       bit_rate;
+
+#ifdef CONFIG_DEVICE_CLOCK
+  struct dev_clock_sink_ep_s     clk_ep;
+#endif
+
+};
+
+static void cc26xx_spi_update_rate(struct cc26xx_spi_context_s *pv)
+{
+  uint32_t div = (pv->freq.num) / (pv->freq.denom * pv->bit_rate);
+  cpu_mem_write_32(pv->addr + CC26XX_SSI_CPSR_ADDR, div);
+}
+
+static DEV_SPI_CTRL_CONFIG(cc26xx_spi_config)
+{
+  struct device_s *dev = accessor->dev;
+  struct cc26xx_spi_context_s *pv = dev->drv_pv;
+  error_t err = 0;
+
+  LOCK_SPIN_IRQ(&dev->lock);
+
+  if (pv->tr != NULL)
+    err = -EBUSY;
+  else
+    {
+      if (cfg->word_width < 4 || cfg->word_width > 16)
+        err = -ENOTSUP;
+      else if (cfg->miso_pol != DEV_SPI_CS_ACTIVE_LOW
+                || cfg->mosi_pol != DEV_SPI_CS_ACTIVE_LOW)
+        err = -ENOTSUP;
+      else if (cfg->bit_order != DEV_SPI_MSB_FIRST)
+        err = -ENOTSUP;
+      else
+        {
+          uint32_t reg = cfg->word_width - 1;
+
+          if (cfg->ck_mode == DEV_SPI_CK_MODE_2 || cfg->ck_mode == DEV_SPI_CK_MODE_3)
+            reg |= CC26XX_SSI_CR0_SPO;
+
+          if (cfg->ck_mode == DEV_SPI_CK_MODE_1 || cfg->ck_mode == DEV_SPI_CK_MODE_3)
+            reg |= CC26XX_SSI_CR0_SPH;
+
+          reg |= CC26XX_SSI_CR0_FRF(MOTOROLA_SPI);
+
+          cpu_mem_write_32(pv->addr + CC26XX_SSI_CR0_ADDR, reg);
+
+          if (pv->bit_rate != cfg->bit_rate)
+            {
+              pv->bit_rate = cfg->bit_rate;
+              cc26xx_spi_update_rate(pv);
+            }
+        }
+    }
+
+  LOCK_RELEASE_IRQ(&dev->lock);
+
+  return err;
+}
+
+static bool_t cc26xx_spi_transfer_tx(struct device_s *dev);
+
+static bool_t cc26xx_spi_transfer_rx(struct device_s *dev)
+{
+  struct cc26xx_spi_context_s *pv = dev->drv_pv;
+  struct dev_spi_ctrl_transfer_s *tr = pv->tr;
+
+  while (pv->fifo_lvl > 0)
+    {
+      /* Check if the rx fifo is empty or not*/
+      uint32_t st = cpu_mem_read_32(pv->addr + CC26XX_SSI_SR_ADDR)
+                      & CC26XX_SSI_SR_RNE;
+
+      if (!st)
+        return 0;           /* wait for more rx irq */
+
+      uint32_t word = cpu_mem_read_32(pv->addr + CC26XX_SSI_DR_ADDR);
+
+      pv->fifo_lvl--;
+
+      if (tr->in == NULL)
+        continue;
+
+      switch (tr->in_width)
+        {
+        case 1:
+          *(uint8_t*)tr->in = word;
+          break;
+        case 2:
+          *(uint16_t*)tr->in = word;
+          break;
+        case 4:
+          *(uint32_t*)tr->in = word;
+          break;
+        }
+
+      tr->in = (void*)((uint8_t*)tr->in + tr->in_width);
+    }
+
+  if (tr->count > 0)
+    return cc26xx_spi_transfer_tx(dev);
+
+  pv->tr = NULL;
+
+  return 1;
+}
+
+static bool_t cc26xx_spi_transfer_tx(struct device_s *dev)
+{
+  struct cc26xx_spi_context_s *pv = dev->drv_pv;
+  struct dev_spi_ctrl_transfer_s *tr = pv->tr;
+
+  while (tr->count > 0 && pv->fifo_lvl < CC26XX_SPI_FIFO_SIZE)
+    {
+      uint32_t word = 0;
+      switch (tr->out_width)
+        {
+        case 1:
+          word = *(const uint8_t*)tr->out;
+          break;
+        case 2:
+          word = *(const uint16_t*)tr->out;
+          break;
+        case 0:
+        case 4:
+          word = *(const uint32_t*)tr->out;
+          break;
+        }
+
+      cpu_mem_write_32(pv->addr + CC26XX_SSI_DR_ADDR, word);
+
+      tr->out = (const void*)((const uint8_t*)tr->out + tr->out_width);
+      tr->count--;
+      pv->fifo_lvl++;
+    }
+
+  return 0;
+}
+
+
+static DEV_IRQ_SRC_PROCESS(cc26xx_spi_irq)
+{
+
+  struct device_s *dev = ep->base.dev;
+  struct cc26xx_spi_context_s *pv = dev->drv_pv;
+
+  lock_spin(&dev->lock);
+
+  while (cpu_mem_read_32(pv->addr + CC26XX_SSI_MIS_ADDR) &
+          (CC26XX_SSI_MIS_RXMIS | CC26XX_SSI_MIS_RTMIS))
+    {
+      struct dev_spi_ctrl_transfer_s *tr = pv->tr;
+
+      if (tr != NULL && cc26xx_spi_transfer_rx(dev))
+        {
+          lock_release(&dev->lock);
+          kroutine_exec(&tr->kr);
+          lock_spin(&dev->lock);
+        }
+    }
+
+  lock_release(&dev->lock);
+}
+
+static DEV_SPI_CTRL_SELECT(cc26xx_spi_select)
+{
+  struct device_s *dev = accessor->dev;
+  struct cc26xx_spi_context_s *pv = dev->drv_pv;
+  error_t err = 0;
+
+  if (cs_id > 0)
+    return -ENOTSUP;
+
+  LOCK_SPIN_IRQ(&dev->lock);
+
+  if (pv->tr != NULL)
+    err = -EBUSY;
+  else
+    {
+      switch (pc)
+        {
+        case DEV_SPI_CS_TRANSFER:
+          break;
+
+        case DEV_SPI_CS_DEASSERT:
+          err = -ENOTSUP;
+          break;
+
+        case DEV_SPI_CS_RELEASE:
+          err = -ENOTSUP;
+          break;
+
+        case DEV_SPI_CS_ASSERT:
+          err = -ENOTSUP;
+          break;
+        }
+    }
+
+  LOCK_RELEASE_IRQ(&dev->lock);
+
+  return err;
+}
+
+static DEV_SPI_CTRL_TRANSFER(cc26xx_spi_transfer)
+{
+  struct device_s *dev = accessor->dev;
+  struct cc26xx_spi_context_s *pv = dev->drv_pv;
+
+  LOCK_SPIN_IRQ(&dev->lock);
+
+  if (pv->tr != NULL)
+    tr->err = -EBUSY;
+  else
+    {
+      assert(tr->count > 0);
+      tr->err = 0;
+      pv->tr = tr;
+      pv->fifo_lvl = 0;
+      cc26xx_spi_transfer_tx(dev);
+    }
+
+  LOCK_RELEASE_IRQ(&dev->lock);
+}
+
+#ifdef CONFIG_DEVICE_SPI_REQUEST
+
+static DEV_SPI_CTRL_QUEUE(cc26xx_spi_queue)
+{
+  struct device_s *dev = accessor->dev;
+  struct cc26xx_spi_context_s *pv = dev->drv_pv;
+  return &pv->queue;
+}
+
+#endif
+
+static DEV_INIT(cc26xx_spi_init);
+static DEV_CLEANUP(cc26xx_spi_cleanup);
+
+#define cc26xx_spi_use dev_use_generic
+
+DRIVER_DECLARE(cc26xx_spi_drv, 0, "CC26XX SPI", cc26xx_spi,
+               DRIVER_SPI_CTRL_METHODS(cc26xx_spi));
+
+DRIVER_REGISTER(cc26xx_spi_drv);
+
+static void power_domain_on(void)
+{
+  uint32_t reg;
+
+  //peripheral power domain on (SSI1)
+  reg = cpu_mem_read_32(CC26XX_PRCM_BASE + CC26XX_PRCM_PDCTL0PERIPH_ADDR);
+  reg |= CC26XX_PRCM_PDCTL0PERIPH_ON;
+  cpu_mem_write_32(CC26XX_PRCM_BASE + CC26XX_PRCM_PDCTL0PERIPH_ADDR, reg);
+
+  //serial power domain on (SSI0)
+  reg = cpu_mem_read_32(CC26XX_PRCM_BASE + CC26XX_PRCM_PDCTL0SERIAL_ADDR);
+  reg |= CC26XX_PRCM_PDCTL0SERIAL_ON;
+  cpu_mem_write_32(CC26XX_PRCM_BASE + CC26XX_PRCM_PDCTL0SERIAL_ADDR, reg);
+
+  //waiting for power
+  while (!(cpu_mem_read_32(CC26XX_PRCM_BASE + CC26XX_PRCM_PDSTAT0_ADDR) &
+    CC26XX_PRCM_PDSTAT0_SERIAL_ON));
+}
+
+static void clk_enable(void)
+{
+  uint32_t reg;
+
+  //SSI clock enable
+  reg = cpu_mem_read_32(CC26XX_PRCM_BASE + CC26XX_PRCM_SSICLKGR_ADDR);
+  reg |= CC26XX_PRCM_SSICLKGR_CLK_EN;
+  cpu_mem_write_32(CC26XX_PRCM_BASE + CC26XX_PRCM_SSICLKGR_ADDR, reg);
+
+  //loading clocks modif
+  reg = CC26XX_PRCM_CLKLOADCTL_LOAD;
+  cpu_mem_write_32(CC26XX_PRCM_NONBUF_BASE + CC26XX_PRCM_CLKLOADCTL_ADDR, reg);
+
+  //waiting for clocks
+  while (!(cpu_mem_read_32(CC26XX_PRCM_BASE + CC26XX_PRCM_CLKLOADCTL_ADDR) &
+    CC26XX_PRCM_CLKLOADCTL_LOAD_DONE));
+}
+
+static DEV_INIT(cc26xx_spi_init)
+{
+  struct cc26xx_spi_context_s *pv;
+
+  device_mem_map( dev , 1 << 0 );
+
+  dev->status = DEVICE_DRIVER_INIT_FAILED;
+
+  pv = mem_alloc(sizeof(*pv), (mem_scope_sys));
+  dev->drv_pv = pv;
+
+  if (!pv)
+    return -ENOMEM;
+
+  memset(pv, 0, sizeof(*pv));
+
+  if (device_res_get_uint(dev, DEV_RES_MEM, 0, &pv->addr, NULL))
+    goto err_mem;
+
+  /* init irq endpoint */
+  device_irq_source_init(dev, &pv->irq_ep, 1, &cc26xx_spi_irq);
+
+  if (device_irq_source_link(dev, &pv->irq_ep, 1, -1))
+    goto err_mem;
+
+  if (device_get_res_freq(dev, &pv->freq, 0))
+    goto err_mem;
+
+  /* init state */
+  pv->tr = NULL;
+
+#ifdef CONFIG_DEVICE_SPI_REQUEST
+  if (dev_spi_queue_init(dev, &pv->queue))
+    goto err_mem;
+#endif
+
+  power_domain_on();
+  clk_enable();
+
+  /* Disable the module*/
+  cpu_mem_write_32(pv->addr + CC26XX_SSI_CR1_ADDR, 0);
+
+  /* Set SPI mode with 8 bits data size */
+  cpu_mem_write_32(pv->addr + CC26XX_SSI_CR0_ADDR, CC26XX_SSI_CR0_DSS(8_BIT)
+                    | CC26XX_SSI_CR0_FRF(MOTOROLA_SPI));
+
+  /* setup pinmux */
+  if (device_iomux_setup(dev, ">clk <miso? >mosi? >cs?", NULL, NULL, NULL))
+    goto err_mem;
+
+  /* setup bit rate */
+  pv->bit_rate = 1000000;
+  cc26xx_spi_update_rate(pv);
+
+  /* Clear interrupt flags */
+  cpu_mem_write_32(pv->addr + CC26XX_SSI_ICR_ADDR, -1);
+
+  /* Set interrupt masks */
+  cpu_mem_write_32(pv->addr + CC26XX_SSI_IMSC_ADDR, CC26XX_SSI_IMSC_RXIM
+                    | CC26XX_SSI_IMSC_RTIM);
+
+  /* Enable the module */
+  cpu_mem_write_32(pv->addr + CC26XX_SSI_CR1_ADDR,
+                   CC26XX_SSI_CR1_SSE(SSI_ENABLED) | CC26XX_SSI_CR1_MS(MASTER));
+
+  dev->drv = &cc26xx_spi_drv;
+  dev->status = DEVICE_DRIVER_INIT_DONE;
+  return 0;
+
+ err_mem:
+  mem_free(pv);
+  return -1;
+}
+
+DEV_CLEANUP(cc26xx_spi_cleanup)
+{
+  struct cc26xx_spi_context_s	*pv = dev->drv_pv;
+
+  if (pv->tr != NULL)
+    return -EBUSY;
+
+  device_irq_source_unlink(dev, &pv->irq_ep, 1);
+
+  /* Clear interrupt flags */
+  cpu_mem_write_32(pv->addr + CC26XX_SSI_ICR_ADDR, -1);
+
+  /* Clear interrupt masks */
+  cpu_mem_write_32(pv->addr + CC26XX_SSI_IMSC_ADDR, 0);
+
+  /* Disable the module */
+  cpu_mem_write_32(pv->addr + CC26XX_SSI_CR1_ADDR, 0);
+
+#ifdef CONFIG_DEVICE_SPI_REQUEST
+  dev_spi_queue_cleanup(&pv->queue);
+#endif
+
+  mem_free(pv);
+
+  return 0;
+}
