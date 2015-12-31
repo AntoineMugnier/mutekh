1d0606ad3b21acd02294336eb449b46d3d564f0b
Alexandre Becoulet <alexandre.becoulet@free.fr>
2015-12-31 16:26:53 +0100

    soclib tty: support multiple ttys

--- arch/soclib/drivers/multi_tty/tty.c
+++ arch/soclib/drivers/multi_tty/tty.c
@@ -41,44 +41,51 @@ GCT_CONTAINER_FCNS(tty_fifo, static inline, tty_fifo,
                    init, destroy, pop_array, pushback, isfull, isempty);
 #endif
 
-struct tty_soclib_context_s
+struct tty_soclib_tty_s
 {
   /* tty input request queue and char fifo */
   dev_request_queue_root_t	read_q;
-  bool_t                        rx_busy;
 #ifdef CONFIG_DEVICE_IRQ
   tty_fifo_root_t		read_fifo;
-  struct dev_irq_src_s          irq_ep;
 #endif
-  uintptr_t addr;
+};
+
+struct tty_soclib_context_s
+{
+  struct dev_irq_src_s          irq_ep;
+  uintptr_t                     addr;
+  uint_fast8_t                  count;
+  struct tty_soclib_tty_s       ttys[0];
 };
 
 #define TTY_SOCLIB_REG_WRITE	0
 #define TTY_SOCLIB_REG_STATUS	4
 #define TTY_SOCLIB_REG_READ	8
+#define TTY_SOCLIB_REG_SPAN	16
 
 static
-void tty_soclib_try_read(struct device_s *dev)
+void tty_soclib_try_read(struct device_s *dev, struct tty_soclib_tty_s *tty)
 {
   struct tty_soclib_context_s	*pv = dev->drv_pv;
+  __unused__ uintptr_t addr = pv->addr + TTY_SOCLIB_REG_SPAN * (tty - pv->ttys);
   struct dev_char_rq_s *rq;
 
-  while ((rq = dev_char_rq_s_cast(dev_request_queue_head(&pv->read_q)))) {
+  while ((rq = dev_char_rq_s_cast(dev_request_queue_head(&tty->read_q)))) {
 
 #ifdef CONFIG_DEVICE_IRQ
-    if (tty_fifo_isempty(&pv->read_fifo))
+    if (tty_fifo_isempty(&tty->read_fifo))
       return;
 
     if (rq->type & _DEV_CHAR_POLL)
       goto done;
 
-    size_t size = tty_fifo_pop_array(&pv->read_fifo, rq->data, rq->size);
+    size_t size = tty_fifo_pop_array(&tty->read_fifo, rq->data, rq->size);
 #else
     /* use polling if no IRQ support available */
     size_t size = 0;
     while (size < rq->size)
-      if (cpu_mem_read_8(pv->addr + TTY_SOCLIB_REG_STATUS))
-        rq->data[size++] = cpu_mem_read_8(pv->addr + TTY_SOCLIB_REG_READ);
+      if ((endian_le32(cpu_mem_read_32(addr + TTY_SOCLIB_REG_STATUS)) & 1)
+        rq->data[size++] = cpu_mem_read_8(addr + TTY_SOCLIB_REG_READ);
 #endif
 
     if (!size)
@@ -90,15 +97,13 @@ void tty_soclib_try_read(struct device_s *dev)
     if (rq->size == 0 || (rq->type & _DEV_CHAR_PARTIAL)) {
     done:
       rq->error = 0;
-      dev_request_queue_pop(&pv->read_q);
+      dev_request_queue_pop(&tty->read_q);
       rq->base.drvdata = NULL;
       lock_release(&dev->lock);
       kroutine_exec(&rq->base.kr);
       lock_spin(&dev->lock);
     }
   }
-
-  pv->rx_busy = 0;
 }
 
 static DEV_CHAR_CANCEL(tty_soclib_cancel)
@@ -106,6 +111,7 @@ static DEV_CHAR_CANCEL(tty_soclib_cancel)
   struct device_s               *dev = accessor->dev;
   struct tty_soclib_context_s	*pv = dev->drv_pv;
   error_t err = -ENOTSUP;
+  struct tty_soclib_tty_s *tty = pv->ttys + accessor->number;
 
   LOCK_SPIN_IRQ(&dev->lock);
 
@@ -114,12 +120,10 @@ static DEV_CHAR_CANCEL(tty_soclib_cancel)
     case DEV_CHAR_READ_PARTIAL:
     case DEV_CHAR_READ:
       err = -EBUSY;
-      if (rq->base.drvdata == pv)
+      if (rq->base.drvdata == tty)
         {
-          rq->error = -ECANCELED;
-          dev_request_queue_remove(&pv->read_q, dev_char_rq_s_base(rq));
+          dev_request_queue_remove(&tty->read_q, dev_char_rq_s_base(rq));
           rq->base.drvdata = NULL;
-          pv->rx_busy = !dev_request_queue_isempty(&pv->read_q);
           err = 0;
         }
     default:
@@ -139,6 +143,8 @@ static DEV_CHAR_REQUEST(tty_soclib_request)
 
   assert(rq->size);
 
+  struct tty_soclib_tty_s *tty = pv->ttys + accessor->number;
+
   LOCK_SPIN_IRQ(&dev->lock);
 
   switch (rq->type)
@@ -150,22 +156,20 @@ static DEV_CHAR_REQUEST(tty_soclib_request)
 #endif
   case DEV_CHAR_READ_PARTIAL:
   case DEV_CHAR_READ:
-    dev_request_queue_pushback(&pv->read_q, dev_char_rq_s_base(rq));
-    rq->base.drvdata = pv;
-    if (pv->rx_busy)
-      break;
-    pv->rx_busy = 1;
-    tty_soclib_try_read(dev);
+    dev_request_queue_pushback(&tty->read_q, dev_char_rq_s_base(rq));
+    rq->base.drvdata = tty;
+    tty_soclib_try_read(dev, tty);
     break;
 
   case DEV_CHAR_WRITE_PARTIAL_FLUSH:
   case DEV_CHAR_WRITE_PARTIAL:
   case DEV_CHAR_WRITE_FLUSH:
   case DEV_CHAR_WRITE: {
+    uintptr_t addr = pv->addr + TTY_SOCLIB_REG_SPAN * accessor->number;
     size_t i;
 
     for (i = 0; i < rq->size; i++)
-      cpu_mem_write_32(pv->addr + TTY_SOCLIB_REG_WRITE, endian_le32(rq->data[i]));
+      cpu_mem_write_32(addr + TTY_SOCLIB_REG_WRITE, endian_le32(rq->data[i]));
 
     rq->size = 0;
   case DEV_CHAR_WRITE_POLL:
@@ -193,23 +197,30 @@ static DEV_IRQ_SRC_PROCESS(tty_soclib_irq)
 {
   struct device_s *dev = ep->base.dev;
   struct tty_soclib_context_s *pv = dev->drv_pv;
+  uint_fast8_t i;
   uint8_t c;
 
   lock_spin(&dev->lock);
 
-  while ( cpu_mem_read_8(pv->addr + TTY_SOCLIB_REG_STATUS) ) {
+  for (i = 0; i < pv->count; i++)
+    {
+      struct tty_soclib_tty_s *tty = pv->ttys + i;
+      uintptr_t addr = pv->addr + TTY_SOCLIB_REG_SPAN * i;
 
-    do {
-      /* get character from tty */
-      c = cpu_mem_read_8(pv->addr + TTY_SOCLIB_REG_READ);
+      while (endian_le32(cpu_mem_read_32(addr + TTY_SOCLIB_REG_STATUS)) & 1) {
 
-      /* add character to driver fifo, discard if fifo full */
-      tty_fifo_pushback(&pv->read_fifo, c);
-    } while ( cpu_mem_read_8(pv->addr + TTY_SOCLIB_REG_STATUS)
-              && ! tty_fifo_isfull(&pv->read_fifo));
+        do {
+          /* get character from tty */
+          c = cpu_mem_read_8(addr + TTY_SOCLIB_REG_READ);
 
-    tty_soclib_try_read(dev);
-  }
+          /* add character to driver fifo, discard if fifo full */
+          tty_fifo_pushback(&tty->read_fifo, c);
+        } while ((endian_le32(cpu_mem_read_32(addr + TTY_SOCLIB_REG_STATUS)) & 1)
+                 && ! tty_fifo_isfull(&tty->read_fifo));
+
+        tty_soclib_try_read(dev, tty);
+      }
+    }
 
   lock_release(&dev->lock);
 }
@@ -219,7 +230,28 @@ static DEV_IRQ_SRC_PROCESS(tty_soclib_irq)
 static DEV_INIT(tty_soclib_init);
 static DEV_CLEANUP(tty_soclib_cleanup);
 
-#define tty_soclib_use dev_use_generic
+static DEV_USE(tty_soclib_use)
+{
+  struct device_accessor_s *accessor = param;
+
+  switch (op)
+    {
+    case DEV_USE_GET_ACCESSOR: {
+      struct tty_soclib_context_s *pv = accessor->dev->drv_pv;
+      if (accessor->number >= pv->count)
+        return -ENOTSUP;
+    }
+    case DEV_USE_PUT_ACCESSOR:
+      return 0;
+    case DEV_USE_LAST_NUMBER: {
+      struct tty_soclib_context_s *pv = accessor->dev->drv_pv;
+      accessor->number = pv->count - 1;
+      return 0;
+    }
+    default:
+      return -ENOTSUP;
+    }
+}
 
 DRIVER_DECLARE(tty_soclib_drv, 0, "Soclib Tty", tty_soclib,
                DRIVER_CHAR_METHODS(tty_soclib));
@@ -234,39 +266,44 @@ static DEV_INIT(tty_soclib_init)
 
   dev->status = DEVICE_DRIVER_INIT_FAILED;
 
-  /* alocate private driver data */
-  pv = mem_alloc(sizeof(*pv), (mem_scope_sys));
-  dev->drv_pv = pv;
+  uintptr_t addr;
+  if (device_res_get_uint(dev, DEV_RES_MEM, 0, &addr, NULL))
+    return -EINVAL;
 
+  uint_fast8_t i, count = endian_le32(cpu_mem_read_32(addr + TTY_SOCLIB_REG_STATUS)) >> 24;
+  if (!count)
+    count = 1;
+
+  pv = mem_alloc(sizeof(*pv) + count * sizeof(struct tty_soclib_tty_s), (mem_scope_sys));
   if (!pv)
     return -ENOMEM;
 
-  dev_request_queue_init(&pv->read_q);
-  pv->rx_busy = 0;
-
-  if (device_res_get_uint(dev, DEV_RES_MEM, 0, &pv->addr, NULL))
-    goto err_mem;
+  dev->drv_pv = pv;
+  pv->addr = addr;
+  pv->count = count;
 
 #ifdef CONFIG_DEVICE_IRQ
   device_irq_source_init(dev, &pv->irq_ep, 1,
                          &tty_soclib_irq);
 
-  tty_fifo_init(&pv->read_fifo);
-
   if (device_irq_source_link(dev, &pv->irq_ep, 1, 1))
-    goto err_fifo;
-
+    goto err_mem;
 #endif
+
+  for (i = 0; i < count; i++)
+    {
+      struct tty_soclib_tty_s *tty = pv->ttys + i;
+      dev_request_queue_init(&tty->read_q);
+#ifdef CONFIG_DEVICE_IRQ
+      tty_fifo_init(&tty->read_fifo);
+#endif
+    }
 
   dev->status = DEVICE_DRIVER_INIT_DONE;
   dev->drv = &tty_soclib_drv;
 
   return 0;
 
-#ifdef CONFIG_DEVICE_IRQ
-  err_fifo:
-  tty_fifo_destroy(&pv->read_fifo);
-#endif
  err_mem:
   mem_free(pv);
   return -1;
@@ -275,17 +312,27 @@ static DEV_INIT(tty_soclib_init)
 static DEV_CLEANUP(tty_soclib_cleanup)
 {
   struct tty_soclib_context_s	*pv = dev->drv_pv;
+  uint_fast8_t i;
 
-  if (pv->rx_busy)
-    return -EBUSY;
+  for (i = 0; i < pv->count; i++)
+    {
+      struct tty_soclib_tty_s *tty = pv->ttys + i;
+      if (!dev_request_queue_isempty(&tty->read_q))
+        return -EBUSY;
+    }
+
+  for (i = 0; i < pv->count; i++)
+    {
+      struct tty_soclib_tty_s *tty = pv->ttys + i;
+#ifdef CONFIG_DEVICE_IRQ
+      tty_fifo_destroy(&tty->read_fifo);
+#endif
+      dev_request_queue_destroy(&tty->read_q);
+    }
 
 #ifdef CONFIG_DEVICE_IRQ
   device_irq_source_unlink(dev, &pv->irq_ep, 1);
-
-  tty_fifo_destroy(&pv->read_fifo);
 #endif
-
-  dev_request_queue_destroy(&pv->read_q);
 
   mem_free(pv);
 
