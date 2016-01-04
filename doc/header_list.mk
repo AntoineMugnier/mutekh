54f29666aad62b03a3e699a640cd64e5e7db9898
Nicolas Pouillon <nipo@ssji.net>
2016-01-04 18:48:24 +0100

    doc: Add libble to doc

--- doc/header_list.mk
+++ doc/header_list.mk
@@ -1,6 +1,6 @@
 
 BASE_MODULES += libfdt libpthread libm libvfs hexo mutek libc   \
-libdevice libnetwork libinet
+libdevice libnetwork libinet libble
 
 ARCH_HEADER= arch/hexo/atomic.h arch/hexo/lock.h	\
 	arch/hexo/types.h
