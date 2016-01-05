b2a4c50daf9d66f1e0c0ebe7bb47590f2e82242f
Alexandre Becoulet <alexandre.becoulet@free.fr>
2016-01-05 16:49:41 +0100

    doc: do not extract mercurial revision in Makefile

--- scripts/doc.mk
+++ scripts/doc.mk
@@ -4,8 +4,6 @@ include $(MUTEK_SRC_DIR)/doc/header_list.mk
 
 include $(MUTEK_SRC_DIR)/scripts/discover.mk
 
-HG_REV:=$(shell cd $(MUTEK_SRC_DIR) && hg summary | grep ^parent | cut -d: -f3 | cut -d' ' -f1)
-
 $(BUILD_DIR)/doc/config.h:
 	test -d $(BUILD_DIR)/doc || mkdir -p $(BUILD_DIR)/doc
 	perl $(MUTEK_SRC_DIR)/scripts/config.pl --src-path=$(MUTEK_SRC_DIR) --docheader=$@
@@ -16,7 +14,6 @@ doc: $(BUILD_DIR)/doc/config.h
 	  --mkdoclib-create mutek-api \
 	  --mkdoclib-url http://www.mutekh.org/www/mutekh_api/ \
 	  --output-path $(BUILD_DIR)/doc \
-	  --source-rev $(HG_REV) \
 	  -I $(BUILD_DIR) -I . \
 	  -I doc/include \
 	  $(subst $(MUTEK_SRC_DIR)/,,$(MKDOC_ARGS))
