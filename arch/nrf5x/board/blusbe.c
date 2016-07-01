e0cf29032811c8138954b98478d0214e04873e70
Nicolas Pouillon <nipo@ssji.net>
2016-07-01 17:54:30 +0200

    arch/nrf5x: Allow blusbe board to use SPI driver

--- arch/nrf5x/board/blusbe.c
+++ arch/nrf5x/board/blusbe.c
@@ -94,6 +94,8 @@ DEV_DECLARE_STATIC(pwm_dev, "leds", 0, nrf5x_gpio_pwm_drv,
 
 #endif
 
+#if defined(CONFIG_DRIVER_NRF52_SPIM) || defined(CONFIG_DRIVER_NRF5X_SPI)
+
 #if defined(CONFIG_DRIVER_NRF52_SPIM)
 
 DEV_DECLARE_STATIC(spi_dev, "spi1", 0, nrf5x_spim_drv,
@@ -106,6 +108,23 @@ DEV_DECLARE_STATIC(spi_dev, "spi1", 0, nrf5x_spim_drv,
                    DEV_STATIC_RES_IOMUX("miso", 0, 28, 0, 0),
                    DEV_STATIC_RES_IOMUX("clk", 0, 30, 0, 0),
                    );
+
+#endif
+
+#if defined(CONFIG_DRIVER_NRF5X_SPI)
+
+DEV_DECLARE_STATIC(spi_dev, "spi1", 0, nrf5x_spi_drv,
+                   NRF_STATIC_RES_PERIPHERAL_MEM(NRF5X_SPI1),
+                   DEV_STATIC_RES_DEV_ICU("/cpu"),
+                   DEV_STATIC_RES_DEV_TIMER("/timer1"),
+                   DEV_STATIC_RES_IRQ(0, NRF5X_SPI1, DEV_IRQ_SENSE_HIGH_LEVEL, 0, 1),
+                   DEV_STATIC_RES_DEV_IOMUX("/gpio"),
+                   DEV_STATIC_RES_IOMUX("mosi", 0, 27, 0, 0),
+                   DEV_STATIC_RES_IOMUX("miso", 0, 28, 0, 0),
+                   DEV_STATIC_RES_IOMUX("clk", 0, 30, 0, 0),
+                   );
+
+#endif
 
 # if defined(CONFIG_DRIVER_USBDEV_MAX3420)
 
