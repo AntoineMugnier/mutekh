d1c1f5fabed8d86a1aa1ba8b7975365f2fea2e61
Vincent Defilippi <vincentdefilippi@gmail.com>
2016-09-14 11:15:23 +0200

    valio: update motion_sensor class

--- libdevice/include/device/valio/motion_sensor.h
+++ libdevice/include/device/valio/motion_sensor.h
@@ -16,6 +16,7 @@
     <http://www.gnu.org/licenses/>.
 
     Copyright Nicolas Pouillon <nipo@ssji.net> (c) 2014
+    Copyright Vincent Defilippi <vincentdefilippi@gmail.com> (c) 2016
 */
 
 /**
@@ -42,15 +43,23 @@ enum valio_ms_axis_e {
     VALIO_MS_AXIS_COUNT,
 };
 
+enum valio_ms_status_e {
+    VALIO_MS_STATUS_INACTIVE,
+    VALIO_MS_STATUS_ACTIVE
+};
+
 enum valio_ms_att_e {
-    /** A struct valio_ms_state_s with current gravity
-        vector, instant rotation and magnetic measurements, read or
-        update */
+    /** A struct valio_ms_state_s with current gravity vector, instant rotation
+        and magnetic measurements.
+        Read or Wait event based on a defined status. */
     VALIO_MS_STATE = CONFIG_DEVICE_VALIO_MOTION_SENSOR_ATTRIBUTE_FIRST,
 
-    /** A ms_state structure with all possible values.  Set offset
-        registers. */
+    /** A struct valio_ms_data with all possible values.
+        Set offset registers. */
     VALIO_MS_CALIBRATE,
+
+    /** A struct valio_ms_accel_config. Read or Write. */
+    VALIO_MS_ACCEL_CONFIG,
 };
 
 /**
@@ -61,9 +70,25 @@ enum valio_ms_att_e {
    Rotation values are in mdps (1e-3 degrees per second).
    Magnetic sensing values are in µT (1e-6 Tesla)
  */
-struct valio_ms_state_s
+struct valio_ms_data_s
 {
   int16_t axis[VALIO_MS_AXIS_COUNT];
+};
+
+struct valio_ms_state_s
+{
+  enum valio_ms_status_e  status;
+  struct valio_ms_data_s  data;
+};
+
+struct valio_ms_accel_config_s
+{
+  uint16_t  active_data_rate_hz;
+  uint16_t  inactive_data_rate_hz;
+  uint16_t  active_threshold_mg;
+  uint16_t  inactive_threshold_mg;
+  uint16_t  active_time_ms;
+  uint16_t  inactive_time_ms;
 };
 
 #endif
