970421a8e79f04022bf7dfcd1c90725b464f90de
Nicolas Pouillon <nipo@ssji.net>
2016-11-29 14:30:16 +0100

    arch/nrf5x: Add IO extender support for PCA100{28,36,40}

--- arch/nrf5x/board/pca10040.c
+++ arch/nrf5x/board/pca10040.c
@@ -23,6 +23,7 @@
 #include <device/class/uart.h>
 #include <device/class/gpio.h>
 #include <device/class/cmu.h>
+#include <device/class/i2c.h>
 #include <arch/nrf5x/ids.h>
 
 #ifdef CONFIG_DRIVER_NRF5X_CLOCK
@@ -77,12 +78,43 @@ DEV_DECLARE_STATIC(uart_dev, "uart0", 0, nrf5x_uart_drv,
 
 #endif
 
-#if defined(CONFIG_DRIVER_NRF5X_GPIO) && defined(CONFIG_DRIVER_BUTTON_SET)
+#if defined(CONFIG_NRF5X_BOARD_I2C_EXT)
+# if defined(CONFIG_DRIVER_NRF5X_I2C)
 
+DEV_DECLARE_STATIC(i2c0_dev, "i2c0", 0, nrf5x_i2c_drv,
+                   NRF_STATIC_RES_PERIPHERAL_MEM(NRF5X_TWI0),
+                   DEV_STATIC_RES_DEV_ICU("/cpu"),
+                   DEV_STATIC_RES_IRQ(0, NRF5X_TWI0, DEV_IRQ_SENSE_HIGH_LEVEL, 0, 1),
+                   DEV_STATIC_RES_DEV_IOMUX("/gpio"),
+                   DEV_STATIC_RES_DEV_TIMER("/timer1"),
+                   DEV_STATIC_RES_DEV_GPIO("/gpio"),
+                   DEV_STATIC_RES_I2C_BITRATE(400000),
+                   DEV_STATIC_RES_IOMUX("scl", 0, 27, 0, 0),
+                   DEV_STATIC_RES_IOMUX("sda", 0, 26, 0, 0)
+                   );
+
+#  if defined(CONFIG_DRIVER_GPIO_PCAL6408A)
+DEV_DECLARE_STATIC(pcal6408a_dev, "gpio1", 0, pcal6408a_drv,
+                   DEV_STATIC_RES_I2C_ADDR("i2c0", 0x20),
+                   DEV_STATIC_RES_DEV_ICU("/gpio"),
+                   DEV_STATIC_RES_IRQ(0, 17, DEV_IRQ_SENSE_LOW_LEVEL, 0, 1),
+                   );
+
+#   if defined(CONFIG_DRIVER_NRF5X_GPIO) && defined(CONFIG_DRIVER_BUTTON_SET)
+DEV_DECLARE_STATIC(keyboard_dev, "keyboard", 0, button_set_drv,
+                     DEV_STATIC_RES_DEV_GPIO("/gpio1"),
+                     DEV_STATIC_RES_GPIO("pins", 0, 4),
+                     DEV_STATIC_RES_UINT_PARAM("active", 0),
+                     );
+#   endif
+#  endif
+# endif
+#else
+# if defined(CONFIG_DRIVER_NRF5X_GPIO) && defined(CONFIG_DRIVER_BUTTON_SET)
 DEV_DECLARE_STATIC(keyboard_dev, "keyboard", 0, button_set_drv,
                    DEV_STATIC_RES_DEV_GPIO("/gpio"),
                    DEV_STATIC_RES_GPIO("pins", 13, 4),
                    DEV_STATIC_RES_UINT_PARAM("active", 0),
                    );
-
+# endif
 #endif
