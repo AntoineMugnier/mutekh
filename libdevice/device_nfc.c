6d6822ae50efa490fcc87f84ada6c693162578f9
Nicolas Pouillon <nipo@ssji.net>
2017-01-14 14:40:16 +0100

    WIP on NFC class

--- libdevice/device_nfc.c
+++ libdevice/device_nfc.c
@@ -34,28 +34,28 @@ extern inline error_t dev_nfc_wait_request(
 
 error_t dev_nfc_wait_transceive_std(
     const struct device_nfc_s *accessor,
-    struct dev_nfc_peer_s *peer,
     const uint8_t *tx_data, size_t tx_size,
-    uint8_t *rx_data, size_t *rx_size)
+    uint8_t *rx_data, size_t *rx_size,
+    enum dev_nfc_div_log2_e div)
 {
   struct dev_nfc_rq_s rq1, rq2;
   struct dev_request_status_s st1, st2;
 
   dev_request_sched_init(&rq1.base, &st1);
-  rq1.peer = peer;
   rq1.type = DEV_NFC_TRANSMIT;
   rq1.data.data = (uint8_t*)tx_data;
   rq1.data.size = tx_size;
   rq1.data.framing = DEV_NFC_FRAMING_STD;
   rq1.data.last_byte_bits = 0;
+  rq1.data.div_log2 = div;
 
   dev_request_sched_init(&rq2.base, &st2);
-  rq2.peer = peer;
   rq2.type = DEV_NFC_RECEIVE;
   rq2.data.data = rx_data;
   rq2.data.size = *rx_size;
   rq2.data.framing = DEV_NFC_FRAMING_STD;
   rq2.data.last_byte_bits = 0;
+  rq2.data.div_log2 = div;
 
   DEVICE_OP(accessor, request, &rq1, &rq2);
 
