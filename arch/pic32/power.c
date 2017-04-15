76049c0a8fe01b522baa69729f78e3ea279be774
Nicolas Pouillon <nipo@ssji.net>
2017-04-15 19:35:26 +0200

    arch/pic32: Implement power API

--- arch/pic32/power.c
+++ arch/pic32/power.c
@@ -17,22 +17,55 @@
     02110-1301 USA.
 
     Copyright Alexandre Becoulet <alexandre.becoulet@free.fr> (c) 2015
-
+    Copyright (c) 2017 Nicolas Pouillon <nipo@ssji.net>
 */
 
 #include <hexo/power.h>
+#include <hexo/iospace.h>
+#include <arch/pic32/devaddr.h>
+#include <arch/pic32/reset.h>
 
 error_t power_reboot()
 {
-  return ENOTSUP;
+  cpu_mem_write_32(PIC32_SYSKEY_ADDR, 0x00000000);
+  cpu_mem_write_32(PIC32_SYSKEY_ADDR, 0xAA996655);
+  cpu_mem_write_32(PIC32_SYSKEY_ADDR, 0x556699AA);
+
+  cpu_mem_write_32(PIC32_RSWRST_ADDR, 1);
+  cpu_mem_read_32(PIC32_RSWRST_ADDR);
+
+  return -ENOTSUP;
 }
 
 error_t power_shutdown()
 {
-  return ENOTSUP;
+  return -ENOTSUP;
 }
 
 enum power_reset_cause_e power_reset_cause(void)
 {
-  return POWER_RESET_CAUSE_UNKNOWN;
+  static enum power_reset_cause_e cause = POWER_RESET_CAUSE_UNKNOWN;
+
+  if (cause == POWER_RESET_CAUSE_UNKNOWN) {
+    uint32_t rcon = cpu_mem_read_32(PIC32_RCON_ADDR);
+
+    cpu_mem_write_32(PIC32_RCON_ADDR, 0);
+  
+    if (rcon & PIC32_RCON_POR)
+      cause = POWER_RESET_CAUSE_POWERUP;
+
+    if (rcon & (PIC32_RCON_EXTR | PIC32_RCON_BOR))
+      cause = POWER_RESET_CAUSE_HARD;
+
+    if (rcon & (PIC32_RCON_WDTO | PIC32_RCON_DMTO))
+      cause = POWER_RESET_CAUSE_WATCHDOG;
+
+    if (rcon & PIC32_RCON_SWR)
+      cause = POWER_RESET_CAUSE_SOFT;
+
+    if (rcon & PIC32_RCON_SLEEP)
+      cause = POWER_RESET_CAUSE_WAKEUP;
+  }
+
+  return cause;
 }
