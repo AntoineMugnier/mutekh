397f7d33a732a9f86a9a3aadfc19098f80147cde
Nicolas Pouillon <nipo@ssji.net>
2018-01-31 15:34:00 +0100

    examples/ble: Add BLE RGB Led example

---
+++ examples/ble/rgb_led/board.c
@@ -0,0 +1,36 @@
+#ifdef CONFIG_DEVICE
+#include <device/resources.h>
+#include <device/class/iomux.h>
+#include <device/class/gpio.h>
+#include <device/class/icu.h>
+#include <arch/nrf5x/ids.h>
+#include <arch/nrf5x/adc.h>
+#endif
+
+#if defined(CONFIG_NRF5X_PCA10000) || defined(CONFIG_NRF5X_PCA10028)
+
+DEV_DECLARE_STATIC(pwm_dev, "leds", 0, nrf5x_gpio_pwm_drv,
+                   NRF_STATIC_RES_PERIPHERAL_MEM(NRF5X_TIMER2),
+                   DEV_STATIC_RES_DEV_ICU("/cpu"),
+                   DEV_STATIC_RES_IRQ(0, NRF5X_TIMER2, DEV_IRQ_SENSE_HIGH_LEVEL, 0, 1),
+                   DEV_STATIC_RES_DEV_IOMUX("/gpio"),
+                   DEV_STATIC_RES_IOMUX("_p0", 0, 21, 0, 0),
+                   DEV_STATIC_RES_IOMUX("_p1", 0, 22, 0, 0),
+                   DEV_STATIC_RES_IOMUX("_p2", 0, 23, 0, 0),
+                   );
+
+#endif
+
+#if defined(CONFIG_NRF5X_PCA10036)
+
+DEV_DECLARE_STATIC(pwm_dev, "leds", 0, nrf5x_gpio_pwm_drv,
+                   NRF_STATIC_RES_PERIPHERAL_MEM(NRF5X_TIMER2),
+                   DEV_STATIC_RES_DEV_ICU("/cpu"),
+                   DEV_STATIC_RES_IRQ(0, NRF5X_TIMER2, DEV_IRQ_SENSE_HIGH_LEVEL, 0, 1),
+                   DEV_STATIC_RES_DEV_IOMUX("/gpio"),
+                   DEV_STATIC_RES_IOMUX("_p0", 0, 17, 0, 0),
+                   DEV_STATIC_RES_IOMUX("_p1", 0, 18, 0, 0),
+                   DEV_STATIC_RES_IOMUX("_p2", 0, 19, 0, 0),
+                   );
+
+#endif
