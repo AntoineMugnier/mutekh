f19810d408b2359bdf1c95ef723dc8b1a6c1b621
Nicolas Pouillon <nipo@ssji.net>
2018-01-31 15:34:32 +0100

    arch/nrf5x: Add BLE_CCM mode to AES driver

--- arch/nrf5x/drivers/aes.c
+++ arch/nrf5x/drivers/aes.c
@@ -40,9 +40,21 @@
 #include <arch/nrf5x/ecb.h>
 #define ECB_ADDR NRF_PERIPHERAL_ADDR(NRF5X_ECB)
 
+#ifdef CONFIG_DRIVER_NRF5X_AES_CCM
+# include <arch/nrf5x/ccm.h>
+# define CCM_ADDR NRF_PERIPHERAL_ADDR(NRF5X_CCM)
+# include <ble/ccm_params.h>
+#endif
+
 DRIVER_PV(struct nrf5x_aes_private_s
 {
   struct dev_request_dlqueue_s queue;
+#ifdef CONFIG_DRIVER_NRF5X_AES_CCM
+  struct dev_irq_src_s irq_ep[1];
+  uint8_t scratch[NRF_CCM_SCRATCH_SIZE];
+  struct dev_crypto_rq_s *current;
+  uint8_t in0;
+#endif
 });
 
 struct nrf5x_aes_state_s
@@ -70,6 +82,10 @@ struct nrf5x_aes_state_s
       size_t size;
     } cmac;
 #endif
+
+#ifdef CONFIG_DRIVER_NRF5X_AES_CCM
+    struct nrf5x_ccm_param_s ccm;
+#endif
   };
 };
 
@@ -85,6 +101,9 @@ static DEV_CRYPTO_INFO(nrf5x_aes_info)
 #endif
 #ifdef CONFIG_DRIVER_NRF5X_AES_CMAC
     | bit(DEV_CRYPTO_MODE_HMAC)
+#endif
+#ifdef CONFIG_DRIVER_NRF5X_AES_CCM
+    | (1 << DEV_CRYPTO_MODE_BLE_CCM)
 #endif
     ;
   info->cap = DEV_CRYPTO_CAP_INPLACE
@@ -308,6 +327,397 @@ static error_t nrf5x_aes_cmac(struct dev_crypto_rq_s *rq)
 }
 #endif
 
+#ifdef CONFIG_DRIVER_NRF5X_AES_CCM
+static void nrf5x_aes_ccm_hw_start(struct nrf5x_aes_private_s *pv,
+                                   struct dev_crypto_rq_s *rq)
+{
+  struct dev_crypto_context_s *ctx = rq->ctx;
+  struct nrf5x_aes_state_s *state = ctx->state_data;
+  const struct ble_ccm_state_s *pstate = (void*)rq->iv_ctr;
+  uint8_t *in = (uint8_t*)rq->in - 1;
+  uint8_t *out = rq->out - 1;
+#if 52000 <= CONFIG_NRF5X_MODEL && CONFIG_NRF5X_MODEL <= 52999
+  uint8_t ciphertext_size = rq->in[1] + (rq->op & DEV_CRYPTO_INVERSE ? 0 : 4);
+#endif
+
+  pv->current = rq;
+
+  state->ccm.packet_counter = pstate->packet_counter;
+  state->ccm.direction = pstate->sent_by_master;
+
+  pv->in0 = in[0];
+  in[0] = in[1];
+  in[1] = in[2];
+  in[2] = 0;
+
+  order_io_mem();
+
+  nrf_reg_set(CCM_ADDR, NRF_CCM_ENABLE, NRF_CCM_ENABLE_ENABLED);
+  nrf_reg_set(CCM_ADDR, NRF_CCM_MODE, ((rq->op & DEV_CRYPTO_INVERSE)
+                                       ? NRF_CCM_MODE_DECRYPTION_FASTEST
+                                       : NRF_CCM_MODE_ENCRYPTION)
+#if 52000 <= CONFIG_NRF5X_MODEL && CONFIG_NRF5X_MODEL <= 52999
+              | (ciphertext_size > 31 ? NRF_CCM_MODE_LENGTH_8BIT : NRF_CCM_MODE_LENGTH_5BIT)
+#endif
+              );
+  nrf_reg_set(CCM_ADDR, NRF_CCM_CNFPTR, (uintptr_t)state);
+  nrf_reg_set(CCM_ADDR, NRF_CCM_INPTR, (uintptr_t)in);
+  nrf_reg_set(CCM_ADDR, NRF_CCM_OUTPTR, (uintptr_t)out);
+  nrf_reg_set(CCM_ADDR, NRF_CCM_SCRATCHPTR, (uintptr_t)pv->scratch);
+
+  nrf_event_clear(CCM_ADDR, NRF_CCM_ERROR);
+  nrf_event_clear(CCM_ADDR, NRF_CCM_ENDCRYPT);
+  nrf_task_trigger(CCM_ADDR, NRF_CCM_KSGEN);
+}
+
+#if NRF_CCM_MAX_SW_PACKET_SIZE != NRF_CCM_MAX_HW_PACKET_SIZE
+static error_t nrf5x_aes_ccm_sw_encrypt(struct nrf5x_aes_private_s *pv,
+                                     struct dev_crypto_rq_s *rq)
+{
+  struct dev_crypto_context_s *ctx = rq->ctx;
+  struct nrf5x_aes_state_s *state = ctx->state_data;
+  const struct ble_ccm_state_s *pstate = (const void *)rq->iv_ctr;
+  struct nrf5x_ecb_param_s aes_ctr, aes_cbc;
+  uint32_t len = rq->in[1];
+
+  if (rq->in[1] > 252)
+    return -EINVAL;
+
+  for (uint32_t i = 0; i < 4; ++i)
+    aes_ctr.key[i] = aes_cbc.key[i] = state->ccm.key[i];
+
+  // Actually, CTR input has same structure than CBC-MAC first block,
+  // reuse context.
+
+  // CBC-MAC block 0 (parameters, IV, packet length)
+  aes_ctr.cleartext[0] = 0x49 | (pstate->packet_counter << 8);
+  aes_ctr.cleartext[1] = (pstate->packet_counter >> 24)
+    | (pstate->sent_by_master ? 0x8000 : 0)
+    | (state->ccm.iv[0] << 16)
+    | (state->ccm.iv[1] << 24);
+  aes_ctr.cleartext[2] = endian_le32_na_load(state->ccm.iv + 2);
+  aes_ctr.cleartext[3] = endian_le16_na_load(state->ccm.iv + 6)
+    | (len << 24);
+  nrf5x_aes_encrypt_start(&aes_ctr);
+  nrf5x_aes_encrypt_spin();
+
+  // CBC-MAC block 1 (AD)
+  aes_cbc.cleartext[0] = aes_ctr.ciphertext[0] ^ (0x100 | ((rq->in[0] & 0xe3) << 16));
+  aes_cbc.cleartext[1] = aes_ctr.ciphertext[1];
+  aes_cbc.cleartext[2] = aes_ctr.ciphertext[2];
+  aes_cbc.cleartext[3] = aes_ctr.ciphertext[3];
+  nrf5x_aes_encrypt_start(&aes_cbc);
+
+  *(uint8_t *)aes_ctr.cleartext = 0x01;
+  uint16_t *ctr = (uint16_t *)aes_ctr.cleartext + 7;
+  *ctr = endian_be16(1);
+
+  nrf5x_aes_encrypt_spin();
+
+  // CTR block 1 (Data cipher stream)
+  nrf5x_aes_encrypt_start(&aes_ctr);
+  
+  for (uint32_t point = 0; point < len; point += 16) {
+    uint32_t input[4];
+
+    // CTR stream generation is running, fetch data in the mean time
+    memcpy(input, rq->in + 2 + point, __MIN(len - point, 16));
+    if (point + 16 > len) {
+      size_t offset = len - point;
+      memset((uint8_t *)input + offset, 0, 16 - offset);
+    }
+
+    // Feedback CBC-MAC input
+    aes_cbc.cleartext[0] = aes_cbc.ciphertext[0] ^ input[0];
+    aes_cbc.cleartext[1] = aes_cbc.ciphertext[1] ^ input[1];
+    aes_cbc.cleartext[2] = aes_cbc.ciphertext[2] ^ input[2];
+    aes_cbc.cleartext[3] = aes_cbc.ciphertext[3] ^ input[3];
+
+    // End of CTR
+    nrf5x_aes_encrypt_spin();
+
+    // Start CBC-MAC on packet data
+    nrf5x_aes_encrypt_start(&aes_cbc);
+
+    // Prepare next counter round
+    if (point + 16 < len)
+      *ctr = endian_be16(point / 16 + 2);
+    else
+      *ctr = 0;
+
+    // Apply stream cipher on packet payload
+    aes_ctr.ciphertext[0] ^= input[0];
+    aes_ctr.ciphertext[1] ^= input[1];
+    aes_ctr.ciphertext[2] ^= input[2];
+    aes_ctr.ciphertext[3] ^= input[3];
+
+    memcpy(rq->out + 2 + point, aes_ctr.ciphertext, __MIN(len - point, 16));
+
+    // End of CBC-MAC
+    nrf5x_aes_encrypt_spin();
+
+    // Next CTR round
+    nrf5x_aes_encrypt_start(&aes_ctr);
+  }    
+
+  // End of CTR block 0, for MIC encoding
+  nrf5x_aes_encrypt_spin();
+
+  rq->out[0] = rq->in[0];
+  rq->out[1] = rq->in[1] + 4;
+  
+  endian_le32_na_store(rq->out + 2 + len, aes_cbc.ciphertext[0] ^ aes_ctr.ciphertext[0]);
+
+  return 0;
+}
+
+static error_t nrf5x_aes_ccm_sw_decrypt(struct nrf5x_aes_private_s *pv,
+                                        struct dev_crypto_rq_s *rq)
+{
+  struct dev_crypto_context_s *ctx = rq->ctx;
+  struct nrf5x_aes_state_s *state = ctx->state_data;
+  const struct ble_ccm_state_s *pstate = (const void *)rq->iv_ctr;
+  struct nrf5x_ecb_param_s aes_ctr, aes_cbc;
+  uint32_t len = rq->in[1] - 4;
+
+  if (rq->in[1] <= 4)
+    return -EINVAL;
+
+  for (uint32_t i = 0; i < 4; ++i)
+    aes_ctr.key[i] = aes_cbc.key[i] = state->ccm.key[i];
+
+  // Actually, CTR input has same structure than CBC-MAC first block,
+  // reuse context.
+
+  // CBC-MAC block 0 (parameters, IV, packet length)
+  aes_ctr.cleartext[0] = 0x49 | (pstate->packet_counter << 8);
+  aes_ctr.cleartext[1] = (pstate->packet_counter >> 24)
+    | (pstate->sent_by_master ? 0x8000 : 0)
+    | (state->ccm.iv[0] << 16)
+    | (state->ccm.iv[1] << 24);
+  aes_ctr.cleartext[2] = endian_le32_na_load(state->ccm.iv + 2);
+  aes_ctr.cleartext[3] = endian_le16_na_load(state->ccm.iv + 6)
+    | (len << 24);
+  nrf5x_aes_encrypt_start(&aes_ctr);
+  nrf5x_aes_encrypt_spin();
+
+  // CBC-MAC block 1 (AD)
+  aes_cbc.cleartext[0] = aes_ctr.ciphertext[0] ^ (0x100 | ((rq->in[0] & 0xe3) << 16));
+  aes_cbc.cleartext[1] = aes_ctr.ciphertext[1];
+  aes_cbc.cleartext[2] = aes_ctr.ciphertext[2];
+  aes_cbc.cleartext[3] = aes_ctr.ciphertext[3];
+  nrf5x_aes_encrypt_start(&aes_cbc);
+
+  *(uint8_t *)aes_ctr.cleartext = 0x01;
+  uint16_t *ctr = (uint16_t *)aes_ctr.cleartext + 7;
+  *ctr = endian_be16(1);
+
+  nrf5x_aes_encrypt_spin();
+
+  // CTR block 1 (Data)
+  nrf5x_aes_encrypt_start(&aes_ctr);
+
+  for (uint32_t point = 0; point < len; point += 16) {
+    uint32_t ciphertext[4];
+    uint32_t cleartext[4];
+
+    // CTR stream generation is running, fetch data in the mean time
+    memcpy(ciphertext, rq->in + 2 + point, __MIN(len - point, 16));
+
+    // End of CTR
+    nrf5x_aes_encrypt_spin();
+
+    // Apply stream cipher on packet payload
+    cleartext[0] = aes_ctr.ciphertext[0] ^ ciphertext[0];
+    cleartext[1] = aes_ctr.ciphertext[1] ^ ciphertext[1];
+    cleartext[2] = aes_ctr.ciphertext[2] ^ ciphertext[2];
+    cleartext[3] = aes_ctr.ciphertext[3] ^ ciphertext[3];
+
+    if (point + 16 > len) {
+      size_t offset = len - point;
+      memset((uint8_t *)cleartext + offset, 0, 16 - offset);
+    }
+
+    // Feedback CBC-MAC input
+    aes_cbc.cleartext[0] = aes_cbc.ciphertext[0] ^ cleartext[0];
+    aes_cbc.cleartext[1] = aes_cbc.ciphertext[1] ^ cleartext[1];
+    aes_cbc.cleartext[2] = aes_cbc.ciphertext[2] ^ cleartext[2];
+    aes_cbc.cleartext[3] = aes_cbc.ciphertext[3] ^ cleartext[3];
+
+    // Start CBC-MAC on packet data
+    nrf5x_aes_encrypt_start(&aes_cbc);
+
+    // Prepare next counter round
+    if (point + 16 < len)
+      *ctr = endian_be16(point / 16 + 2);
+    else
+      *ctr = 0;
+
+    memcpy(rq->out + 2 + point, cleartext, __MIN(len - point, 16));
+
+    // End of CBC-MAC
+    nrf5x_aes_encrypt_spin();
+
+    // Next CTR round
+    nrf5x_aes_encrypt_start(&aes_ctr);
+  }    
+
+  // End of CTR block 0, for MIC encoding
+  nrf5x_aes_encrypt_spin();
+
+  rq->out[0] = rq->in[0];
+  rq->out[1] = rq->in[1] - 4;
+
+  uint32_t mic = aes_cbc.ciphertext[0] ^ aes_ctr.ciphertext[0];
+
+  if (endian_le32_na_load(rq->in + 2 + len) == mic)
+    return 0;
+
+#if 0
+  printk("MIC Status failed, expected %08x, got %08x:\n",
+         endian_le32_na_load(rq->in + 2 + len), mic);
+  printk(" In:  %P\n", rq->in, rq->in[1] + 6);
+  printk(" Out: %P\n", rq->out, rq->out[1] + 2);
+#endif
+
+  return -EINVAL;
+}
+#endif
+
+static bool_t nrf5x_aes_ccm(struct nrf5x_aes_private_s *pv,
+                            struct dev_crypto_rq_s *rq)
+{
+  struct dev_crypto_context_s *ctx = rq->ctx;
+  struct nrf5x_aes_state_s *state = ctx->state_data;
+
+  if (!state) {
+    rq->err = -EINVAL;
+    return 0;
+  }
+
+  rq->err = -ENOTSUP;
+
+  if (rq->op & DEV_CRYPTO_INIT) {
+    memcpy(state->ccm.key, ctx->key_data, 16);
+    memcpy(state->ccm.iv, rq->iv_ctr, 8);
+    rq->err = 0;
+  }
+
+  if (rq->op & DEV_CRYPTO_FINALIZE) {
+    uint8_t cleartext_size = rq->in[1] - (rq->op & DEV_CRYPTO_INVERSE ? 4 : 0);
+
+    if (cleartext_size <= NRF_CCM_MAX_HW_PACKET_SIZE) {
+      rq->err = -EAGAIN;
+      nrf5x_aes_ccm_hw_start(pv, rq);
+      return 1;
+    }
+
+#if NRF_CCM_MAX_SW_PACKET_SIZE != NRF_CCM_MAX_HW_PACKET_SIZE
+    if (rq->op & DEV_CRYPTO_INVERSE)
+      rq->err = nrf5x_aes_ccm_sw_decrypt(pv, rq);
+    else
+      rq->err = nrf5x_aes_ccm_sw_encrypt(pv, rq);
+#else
+    rq->err = -ENOTSUP;
+#endif
+  }
+
+  return 0;
+}
+
+static DEV_IRQ_SRC_PROCESS(nrf5x_aes_irq)
+{
+  struct device_s *dev = ep->base.dev;
+  struct nrf5x_aes_private_s *pv = dev->drv_pv;
+
+  lock_spin(&dev->lock);
+
+  for (;;) {
+    if (nrf_event_check(CCM_ADDR, NRF_CCM_ERROR)) {
+      nrf_event_clear(CCM_ADDR, NRF_CCM_ERROR);
+
+      printk("CCM bus conflict, restarting\n");
+      nrf_event_clear(CCM_ADDR, NRF_CCM_ENDCRYPT);
+      nrf_task_trigger(CCM_ADDR, NRF_CCM_KSGEN);
+
+      continue;
+    }
+
+    if (nrf_event_check(CCM_ADDR, NRF_CCM_ENDCRYPT)) {
+      nrf_event_clear(CCM_ADDR, NRF_CCM_ENDCRYPT);
+
+      if (!pv->current) {
+        printk("Spurious CCM IRQ\n");
+        continue;
+      }
+
+      struct dev_crypto_rq_s *rq = pv->current;
+      struct dev_crypto_context_s *ctx = rq->ctx;
+      __unused__ struct nrf5x_aes_state_s *state = ctx->state_data;
+
+      if ((rq->op & DEV_CRYPTO_INVERSE)
+          && nrf_reg_get(CCM_ADDR, NRF_CCM_MICSTATUS) == NRF_CCM_MICSTATUS_FAILED
+          && ((nrf_reg_get(CCM_ADDR, NRF_CCM_MODE) & NRF_CCM_MODE_MASK)
+              == NRF_CCM_MODE_DECRYPTION_FASTEST)) {
+
+        printk("Fast CCM decrypt failed, starting again in slow mode\n");
+        nrf_reg_set(CCM_ADDR, NRF_CCM_MODE,
+                    (nrf_reg_get(CCM_ADDR, NRF_CCM_MODE) & ~NRF_CCM_MODE_MASK)
+                    | NRF_CCM_MODE_DECRYPTION);
+
+        nrf_event_clear(CCM_ADDR, NRF_CCM_ENDCRYPT);
+        nrf_event_clear(CCM_ADDR, NRF_CCM_ERROR);
+        nrf_task_trigger(CCM_ADDR, NRF_CCM_KSGEN);
+
+        continue;
+      }
+
+      pv->current = NULL;
+
+      uint8_t *in = (uint8_t*)rq->in - 1;
+      uint8_t *out = rq->out - 1;
+
+      in[2] = in[1];
+      in[1] = in[0];
+      in[0] = pv->in0;
+
+      out[2] = out[1];
+      out[1] = out[0];
+
+      if (nrf_reg_get(CCM_ADDR, NRF_CCM_MICSTATUS) == NRF_CCM_MICSTATUS_FAILED) {
+        if (rq->op & DEV_CRYPTO_INVERSE) {
+#if 1
+          printk("MIC Status failed:\n");
+          printk(" In:  %P\n", rq->in, rq->in[1] + 2);
+          printk(" Out: %P\n", rq->out, rq->out[1] + 2);
+          printk(" Ctx: %P\n", state, sizeof(*state));
+          printk(" Scr: %P\n", pv->scratch, sizeof(pv->scratch));
+#endif  
+          rq->err = -EINVAL;
+        } else {
+          rq->err = 0;
+        }
+      } else {
+        rq->err = 0;
+      }
+
+      /* order_io_mem(); */
+
+      nrf_reg_set(CCM_ADDR, NRF_CCM_ENABLE, NRF_CCM_ENABLE_DISABLED);
+
+      lock_release(&dev->lock);
+      dev_request_delayed_end(&pv->queue, &rq->base);
+      lock_spin(&dev->lock);
+      continue;
+    }
+
+    break;
+  }
+  
+  lock_release(&dev->lock);
+}
+#endif
+
 static DEV_REQUEST_DELAYED_FUNC(nrf5x_aes_process)
 {
   struct dev_crypto_rq_s *rq = dev_crypto_rq_s_cast(rq_);
@@ -337,6 +747,12 @@ static DEV_REQUEST_DELAYED_FUNC(nrf5x_aes_process)
 #ifdef CONFIG_DRIVER_NRF5X_AES_CMAC
   case DEV_CRYPTO_MODE_HMAC:
     rq->err = nrf5x_aes_cmac(rq);
+    break;
+#endif
+
+#ifdef CONFIG_DRIVER_NRF5X_AES_CCM
+  case DEV_CRYPTO_MODE_BLE_CCM:
+    delayed = nrf5x_aes_ccm(pv, rq);
     break;
 #endif
   }
@@ -371,6 +787,20 @@ static DEV_INIT(nrf5x_aes_init)
 
   memset(pv, 0, sizeof(*pv));
 
+#ifdef CONFIG_DRIVER_NRF5X_AES_CCM
+  device_irq_source_init(dev, pv->irq_ep, 1, &nrf5x_aes_irq);
+
+  if (device_irq_source_link(dev, pv->irq_ep, 1, -1))
+    goto err_mem;
+
+  nrf_it_disable_mask(CCM_ADDR, -1);
+
+  nrf_it_enable_mask(CCM_ADDR, 0
+                     | (1 << NRF_CCM_ENDCRYPT)
+                     | (1 << NRF_CCM_ERROR));
+  nrf_short_enable_mask(CCM_ADDR, 1 << NRF_CCM_ENDKSGEN_CRYPT);
+#endif
+
   dev->drv_pv = pv;
 
   dev_request_delayed_init(&pv->queue, &nrf5x_aes_process);
@@ -391,6 +821,11 @@ static DEV_CLEANUP(nrf5x_aes_cleanup)
 
   dev_request_delayed_cleanup(&pv->queue);
 
+#ifdef CONFIG_DRIVER_NRF5X_AES_CCM
+  nrf_it_disable_mask(CCM_ADDR, -1);
+  device_irq_source_unlink(dev, pv->irq_ep, 1);
+#endif
+
   mem_free(pv);
 
   return 0;
@@ -403,8 +838,10 @@ DRIVER_DECLARE(nrf5x_aes_drv, 0, "nRF5x AES"
 #ifdef CONFIG_DRIVER_NRF5X_AES_CMAC
                ",CMAC"
 #endif
+#ifdef CONFIG_DRIVER_NRF5X_AES_CCM
+               ",CCM"
+#endif
                , nrf5x_aes,
                DRIVER_CRYPTO_METHODS(nrf5x_aes));
 
 DRIVER_REGISTER(nrf5x_aes_drv);
-
