df3805db9a428181834cdf4c24ebcf05dbdcb95f
Alexandre Becoulet <alexandre.becoulet@free.fr>
2018-04-22 18:49:23 +0200

    hexo: added default implementations for flash_page_erase and flash_page_write

---
+++ hexo/flash.c
@@ -0,0 +1,37 @@
+/*
+    This file is part of MutekH.
+    
+    MutekH is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; version 2.1 of the
+    License.
+    
+    MutekH is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+    
+    You should have received a copy of the GNU Lesser General Public
+    License along with MutekH; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301 USA.
+
+    Copyright Alexandre Becoulet <alexandre.becoulet@lip6.fr> (c) 2006
+
+*/
+
+#include <hexo/flash.h>
+
+__attribute__((weak))
+reg_t flash_page_erase(uintptr_t addr)
+{
+  return 1;
+}
+
+__attribute__((weak))
+reg_t flash_page_write(uintptr_t addr,
+                       const uint8_t *data,
+                       size_t size)
+{
+  return 1;
+}
