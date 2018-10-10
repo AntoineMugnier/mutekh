43c12bc2fda1b516d05db5fd5fdc5e2792952ffc
Pierre Bertin <bertinp17@icloud.com>
2018-10-10 15:26:28 +0200

    nrf5x: add ACL headers

---
+++ arch/nrf5x/include/arch/nrf5x/acl.h
@@ -0,0 +1,33 @@
+/*
+    This file is part of MutekH.
+
+    MutekH is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; version 2.1 of the
+    License.
+
+    MutekH is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program.  If not, see
+    <http://www.gnu.org/licenses/>.
+
+    Copyright Govak Lamesi (c) 2018
+    Copyright Pierre Bertin <glihm@free.fr> (c) 2018
+*/
+
+#ifndef ARCH_NRF_ACL_H_
+#define ARCH_NRF_ACL_H_
+
+#include "peripheral.h"
+#include "ids.h"
+
+enum nrf5x_acl_perm {
+  NRF_ACL_WRITE = 2,
+  NRF_ACL_READ  = 4,
+};
+
+#endif  /* ARCH_NRF_RTC_H_ */
