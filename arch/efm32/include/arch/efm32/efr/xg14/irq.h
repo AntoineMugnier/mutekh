e85d85105090a371ac91cf5dde18074c267c0551
sebcerdan <unknown>
2020-09-08 10:16:55 +0200

    EFR32XG14: Fix bad peripheral IRQ index

---
+++ arch/efm32/include/arch/efm32/efr/xg14/irq.h
@@ -0,0 +1,47 @@
+#ifndef IRQ_H_
+# define IRQ_H_
+
+#define EFM32_IRQ_EMU            0
+#define EFM32_IRQ_WDOG0          2
+#define EFM32_IRQ_WDOG1          3
+#define EFM32_IRQ_FRC            4
+#define EFM32_IRQ_MODEM          5
+#define EFM32_IRQ_RAC_SEQ        6
+#define EFM32_IRQ_RAC_RSM        7
+#define EFM32_IRQ_BUFC           8
+#define EFM32_IRQ_DMA            9
+#define EFM32_IRQ_GPIO_EVEN      10
+#define EFM32_IRQ_TIMER0         11
+#define EFM32_IRQ_USART0_RX      12
+#define EFM32_IRQ_USART0_TX      13
+#define EFM32_IRQ_ACMP0          14
+#define EFM32_IRQ_ADC0           15
+#define EFM32_IRQ_IDAC0          16
+#define EFM32_IRQ_I2C0           17
+#define EFM32_IRQ_GPIO_ODD       18
+#define EFM32_IRQ_TIMER1         19
+#define EFM32_IRQ_USART1_RX      20
+#define EFM32_IRQ_USART1_TX      21
+#define EFM32_IRQ_LEUART0        22
+#define EFM32_IRQ_PCNT0          23
+#define EFM32_IRQ_CMU            24
+#define EFM32_IRQ_MSC            25
+#define EFM32_IRQ_CRYPTO         26
+#define EFM32_IRQ_LETIMER0       27
+#define EFM32_IRQ_AGC            28
+#define EFM32_IRQ_PROTIMER       29
+#define EFM32_IRQ_PRORTC         30
+#define EFM32_IRQ_RTCC           31
+#define EFM32_IRQ_SYNTH          32
+#define EFM32_IRQ_CRYOTIMER      33
+#define EFM32_IRQ_RFSENSE        34
+#define EFM32_IRQ_FPUEH          35
+#define EFM32_IRQ_SMU            36
+#define EFM32_IRQ_WTIMER0        37
+#define EFM32_IRQ_USART2_RX      40
+#define EFM32_IRQ_USART2_TX      41
+#define EFM32_IRQ_USART3_RX      43
+#define EFM32_IRQ_USART3_TX      44
+
+
+#endif /* !IRQ_H_ */
