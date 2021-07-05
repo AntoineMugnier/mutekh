a5743520f4f467953c596e7544aeeb3d2aadf8ed
Jean-Roland <jean.roland.gosse@gmail.com>
2021-07-05 15:20:29 +0200

    efm32: fix: clear rq error when  gpio wait until success

--- arch/efm32/drivers/gpio/gpio.c
+++ arch/efm32/drivers/gpio/gpio.c
@@ -727,6 +727,7 @@ static DEV_IRQ_SRC_PROCESS(efm32_gpio_source_process)
               x ^= m;
               efm32_gpio_until_clear(m);
               rq->drvuint = 0;
+              rq->error = 0;
               dev_gpio_rq_remove(&pv->queue, dev_gpio_rq_s_cast(rq));
               dev_gpio_rq_done(dev_gpio_rq_s_cast(rq));
             }
