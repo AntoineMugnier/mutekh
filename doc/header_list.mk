4d39ecbc24fc359f7bcc6ab85094bcd15e404165
Alexandre Becoulet <alexandre.becoulet@free.fr>
2022-08-04 16:10:54 +0200

    fix doc build

--- doc/header_list.mk
+++ doc/header_list.mk
@@ -1,6 +1,6 @@
 
 BASE_MODULES += libfdt libpthread libm libvfs hexo mutek libc   \
-libdevice libnetwork libinet libble libpersist libgfx
+libdevice libnetwork libinet libble libpersist libgfx libvulpis
 
 ARCH_HEADER= arch/hexo/atomic.h arch/hexo/lock.h	\
 	arch/hexo/types.h
