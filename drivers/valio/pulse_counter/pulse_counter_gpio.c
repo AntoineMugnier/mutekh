8dc3492891b29d852031bc0721d3b4ae1bf5446b
Nicolas Pouillon <nipo@ssji.net>
2023-06-10 14:44:35 +0200

    drivers: Add GPIO pulse counter

---
+++ drivers/valio/pulse_counter/pulse_counter_gpio.c
@@ -0,0 +1,287 @@
+/*
+    This file is part of MutekH.
+
+    MutekH is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; version 2.1 of the
+    License.
+
+    MutekH is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program.  If not, see
+    <http://www.gnu.org/licenses/>.
+
+    Copyright (c) Nicolas Pouillon <nipo@ssji.net> 2021
+*/
+
+#define LOGK_MODULE_ID "pcgp"
+
+#include <hexo/types.h>
+#include <hexo/endian.h>
+#include <hexo/iospace.h>
+#include <hexo/bit.h>
+
+#include <mutek/mem_alloc.h>
+#include <mutek/printk.h>
+
+#include <device/resources.h>
+#include <device/device.h>
+#include <device/driver.h>
+
+#include <device/class/gpio.h>
+#include <device/class/iomux.h>
+#include <device/class/valio.h>
+#include <device/valio/position.h>
+
+struct pc_pv_s
+{
+  dev_request_queue_root_t queue;
+  struct device_gpio_s gpio;
+  struct dev_gpio_rq_s gpio_rq;
+
+  uint8_t cur[8];
+
+  bool_t last_state;
+  uint32_t count;
+};
+
+DRIVER_PV(struct pc_pv_s);
+
+static void pc_gpio_read(struct device_s *dev)
+{
+  struct pc_pv_s *pv = dev->drv_pv;
+
+  pv->gpio_rq.type = DEV_GPIO_GET_INPUT;
+  pv->gpio_rq.input.data = pv->cur;
+  
+  DEVICE_OP(&pv->gpio, request, &pv->gpio_rq);
+}
+
+static void pc_gpio_wait(struct device_s *dev)
+{
+  struct pc_pv_s *pv = dev->drv_pv;
+
+  pv->gpio_rq.type = DEV_GPIO_UNTIL;
+  pv->gpio_rq.until.data = pv->cur;
+  pv->gpio_rq.until.mask = dev_gpio_mask1;
+
+  DEVICE_OP(&pv->gpio, request, &pv->gpio_rq);
+}
+
+static
+void pc_update(struct device_s *dev)
+{
+  struct pc_pv_s *pv = dev->drv_pv;
+
+  uint8_t last_state = pv->last_state;
+  uint8_t state = pv->cur[0] & 1;
+
+  if (!last_state && state)
+    pv->count++;
+
+  logk_trace("%d->%d, count %d", last_state, state, pv->count);
+
+  uint32_t count = pv->count;
+  pv->last_state = state;
+
+  GCT_FOREACH(dev_request_queue, &pv->queue, item, {
+      struct dev_valio_rq_s *rq = dev_valio_rq_s_cast(item);
+      int32_t *rq_value = rq->data;
+
+      if (rq->type == DEVICE_VALIO_WAIT_EVENT
+          && *rq_value == count) {
+        logk_debug("rq %p not changed", rq);
+        GCT_FOREACH_CONTINUE;
+      }
+
+      *rq_value = count;
+
+      dev_valio_rq_remove(&pv->queue, rq);
+      dev_valio_rq_done(rq);
+    });
+}
+
+static KROUTINE_EXEC(pc_gpio_done)
+{
+  struct pc_pv_s *pv = KROUTINE_CONTAINER(kr, *pv, gpio_rq.base.kr);
+  struct device_s *dev = pv->gpio_rq.pvdata;
+
+  LOCK_SPIN_IRQ_SCOPED(&dev->lock);
+
+  switch (pv->gpio_rq.type) {
+  default:
+  case DEV_GPIO_MODE:
+    // Pin init done, read input once
+    pc_gpio_read(dev);
+    break;
+
+  case DEV_GPIO_GET_INPUT:
+    // Init done
+    pc_gpio_wait(dev);
+    pc_update(dev);
+    break;
+
+  case DEV_GPIO_UNTIL:
+    pc_gpio_read(dev);
+    break;
+  }
+}
+
+static DEV_VALIO_REQUEST(pc_request)
+{
+  struct device_s *dev = accessor->dev;
+  struct pc_pv_s *pv = dev->drv_pv;
+
+  logk_debug("%s", __func__);
+
+  LOCK_SPIN_IRQ_SCOPED(&dev->lock);
+
+  if (rq->attribute != VALIO_POSITION_VALUE)
+    goto inval;
+
+  uint32_t *rq_ptr = (uint32_t*)rq->data;
+  uint32_t count = pv->count;
+
+  switch (rq->type) {
+  case DEVICE_VALIO_WAIT_EVENT:
+    if (*rq_ptr == count) {
+      logk_debug("wait %d = %d queued", *rq_ptr, count);
+      rq->error = 0;
+      dev_valio_rq_pushback(&pv->queue, rq);
+      return;
+    }
+
+    // fallthrough
+  case DEVICE_VALIO_READ:
+    *rq_ptr = count;
+    goto done;
+
+  case DEVICE_VALIO_WRITE:
+    pv->count = *rq_ptr;
+
+  done:
+    rq->error = 0;
+    dev_valio_rq_done(rq);
+    return;
+
+  default:
+  inval:
+    rq->error = -EINVAL;
+    logk_debug("inval");
+    dev_valio_rq_done(rq);
+    return;
+  }
+}
+
+static DEV_VALIO_CANCEL(pc_cancel)
+{
+  struct device_s *dev = accessor->dev;
+  struct pc_pv_s *pv = dev->drv_pv;
+
+  logk_trace("%s %p", __func__, rq);
+
+  LOCK_SPIN_IRQ_SCOPED(&dev->lock);
+
+  GCT_FOREACH(dev_request_queue, &pv->queue, item, {
+      struct dev_valio_rq_s *rq_item = dev_valio_rq_s_cast(item);
+      if (rq == rq_item) {
+        dev_valio_rq_remove(&pv->queue, rq);
+        return 0;
+      }
+    });
+
+  return -ENOENT;
+}
+
+#define pc_use dev_use_generic
+
+static DEV_INIT(pc_init)
+{
+  struct pc_pv_s *pv;
+  error_t err;
+  gpio_id_t id;
+  gpio_width_t width;
+  uintptr_t tmp;
+
+  pv = mem_alloc(sizeof(*pv), mem_scope_sys);
+  if (!pv)
+    return -ENOMEM;
+
+  memset(pv, 0, sizeof(*pv));
+
+  dev->drv_pv = pv;
+
+  err = device_get_param_dev_accessor(dev, "gpio", &pv->gpio.base, DRIVER_CLASS_GPIO);
+  if (err) {
+    logk_error("Cannot get GPIO accessor");
+    goto free_pv;
+  }
+
+  err = device_gpio_get_setup(&pv->gpio, dev, "pin", &id, &width);
+  if (err) {
+    logk_error("No 'pin' param");
+    goto put_gpio;
+  }
+
+  if (width != 1) {
+    logk_error("Width is supposed to be 1");
+    goto put_gpio;
+  }
+
+  dev_rq_queue_init(&pv->queue);
+  dev_gpio_rq_init(&pv->gpio_rq, pc_gpio_done);
+
+  pv->gpio_rq.pvdata = dev;
+  pv->gpio_rq.io_first = id;
+  pv->gpio_rq.io_last = id;
+  err = device_get_param_uint(dev, "pull", &tmp);
+  if (!err && tmp)
+    pv->gpio_rq.mode.mode = DEV_PIN_INPUT_PULLUP;
+  else
+    pv->gpio_rq.mode.mode = DEV_PIN_INPUT;
+  pv->gpio_rq.mode.mask = dev_gpio_mask1;
+  pv->gpio_rq.type = DEV_GPIO_MODE;
+
+  logk("Pin %d mode %d", id, pv->gpio_rq.mode.mode);
+
+  DEVICE_OP(&pv->gpio, request, &pv->gpio_rq);
+  
+  return 0;
+
+ put_gpio:
+  device_put_accessor(&pv->gpio.base);
+ free_pv:
+  mem_free(pv);
+  return err;
+}
+
+static DEV_CLEANUP(pc_cleanup)
+{
+  struct pc_pv_s *pv = dev->drv_pv;
+
+  DEVICE_OP(&pv->gpio, cancel, &pv->gpio_rq);
+
+  GCT_FOREACH(dev_request_queue, &pv->queue, item, {
+      struct dev_valio_rq_s *rq = dev_valio_rq_s_cast(item);
+      rq->error = -EIO;
+      dev_valio_rq_remove(&pv->queue, rq);
+      dev_valio_rq_done(rq);
+    });
+
+  device_put_accessor(&pv->gpio.base);
+  dev_rq_queue_destroy(&pv->queue);
+
+  mem_free(pv);
+
+  return 0;
+}
+
+DRIVER_DECLARE(pc_gpio_drv, 0, "Pulse counter", pc,
+               DRIVER_VALIO_METHODS(pc));
+
+DRIVER_REGISTER(pc_gpio_drv);
+
