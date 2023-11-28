42ad6688e6b1ca5b21dec0e97c6019f3871a2023
Nicolas Pouillon <nipo@ssji.net>
2023-11-28 17:15:54 +0100

    drivers/printk/console: Avoid deadlocking when console driver does printk() calls

--- drivers/printk/console.c
+++ drivers/printk/console.c
@@ -24,57 +24,70 @@
 
 #include <device/class/char.h>
 #include <mutek/console.h>
+#include <mutek/kroutine.h>
 
-struct console_printk_status_s {
+struct console_printk_status_s
+{
   char fifo[CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE];
   size_t use_begin, use_end, use_size;
   size_t free_begin, free_end, free_size;
   size_t pending_tx_size;
-  struct lock_irq_s lock;
+  struct lock_s lock;
   struct dev_char_rq_s char_rq;
   struct printk_backend_s backend;
+  struct kroutine_s service;
 };
 
 STRUCT_COMPOSE(console_printk_status_s, backend)
+  STRUCT_COMPOSE(console_printk_status_s, char_rq)
+  STRUCT_COMPOSE(console_printk_status_s, service)
 
-static void console_printk_rq_next(struct console_printk_status_s *pv)
+  static KROUTINE_EXEC(console_printk_service)
 {
+  struct console_printk_status_s *pv = console_printk_status_s_from_service(kr);
   size_t rptr;
 
-  assert(!pv->pending_tx_size);
+  {
+    LOCK_SPIN_IRQ_SCOPED(&pv->lock);
 
-  pv->pending_tx_size = __MIN(pv->use_size,
-                 CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE - pv->use_begin);
+    if (pv->pending_tx_size)
+      return;
 
-  if (!pv->pending_tx_size)
-    return;
+    pv->pending_tx_size = __MIN(pv->use_size,
+                                CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE - pv->use_begin);
 
-  rptr = pv->use_begin;
-  pv->use_size -= pv->pending_tx_size;
-  pv->use_begin += pv->pending_tx_size;
-  if (pv->use_begin == CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE)
-    pv->use_begin = 0;
+    if (!pv->pending_tx_size)
+      return;
 
-  pv->char_rq.data = (void*)(pv->fifo + rptr);
-  pv->char_rq.size = pv->pending_tx_size;
+    rptr = pv->use_begin;
+    pv->use_size -= pv->pending_tx_size;
+    pv->use_begin += pv->pending_tx_size;
+    if (pv->use_begin == CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE)
+      pv->use_begin = 0;
 
+    pv->char_rq.data = (void*)(pv->fifo + rptr);
+    pv->char_rq.size = pv->pending_tx_size;
+  }
+  
   DEVICE_OP(&console_dev, request, &pv->char_rq);
 }
 
 static KROUTINE_EXEC(console_printk_done)
 {
-  struct console_printk_status_s *pv = KROUTINE_CONTAINER(kr, *pv, char_rq.base.kr);
+  struct dev_char_rq_s *rq = dev_char_rq_from_kr(kr);
+  struct console_printk_status_s *pv = console_printk_status_s_from_char_rq(rq);
+
+  {
+    LOCK_SPIN_IRQ_SCOPED(&pv->lock);
+    pv->free_size += pv->pending_tx_size;
+    pv->free_end += pv->pending_tx_size;
+    pv->pending_tx_size = 0;
+
+    if (pv->free_begin >= CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE)
+      pv->free_begin -= CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE;
+  }
   
-  lock_spin_irq(&pv->lock);
-  pv->free_size += pv->pending_tx_size;
-  pv->free_end += pv->pending_tx_size;
-  if (pv->free_begin >= CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE)
-    pv->free_begin -= CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE;
-
-  pv->pending_tx_size = 0;
-  console_printk_rq_next(pv);
-
-  lock_release_irq(&pv->lock);
+  kroutine_exec(&pv->service);
 }
 
 static PRINTK_HANDLER(console_printk_out)
@@ -88,16 +101,17 @@ static PRINTK_HANDLER(console_printk_out)
   for (;;) {
     size_t wptr, copied;
 
-    lock_spin_irq(&pv->lock);
-    copied = __MIN(__MIN(len, pv->free_size),
-                   CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE - pv->free_begin);
+    {
+      LOCK_SPIN_IRQ_SCOPED(&pv->lock);
+      copied = __MIN(__MIN(len, pv->free_size),
+                     CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE - pv->free_begin);
 
-    wptr = pv->free_begin;
-    pv->free_size -= copied;
-    pv->free_begin += copied;
-    if (pv->free_begin == CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE)
-      pv->free_begin = 0;
-    lock_release_irq(&pv->lock);
+      wptr = pv->free_begin;
+      pv->free_size -= copied;
+      pv->free_begin += copied;
+      if (pv->free_begin == CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE)
+        pv->free_begin = 0;
+    }
 
     if (!copied)
       break;
@@ -106,23 +120,25 @@ static PRINTK_HANDLER(console_printk_out)
     str += copied;
     len -= copied;
 
-    lock_spin_irq(&pv->lock);
-    pv->use_size += copied;
-    pv->use_end += copied;
-    if (pv->free_begin >= CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE)
-      pv->free_begin -= CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE;
-
-    if (!pv->pending_tx_size)
-      console_printk_rq_next(pv);
-    lock_release_irq(&pv->lock);
+    {
+      LOCK_SPIN_IRQ_SCOPED(&pv->lock);
+      pv->use_size += copied;
+      pv->use_end += copied;
+      if (pv->free_begin >= CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE)
+        pv->free_begin -= CONFIG_DRIVER_CONSOLE_PRINTK_BUFFER_SIZE;
+    }
   }
+
+  kroutine_exec(&pv->service);
 }
 
 void console_printk_init(void)
 {
   static struct console_printk_status_s status;
 
-  lock_init_irq(&status.lock);
+  kroutine_init_idle(&status.service, console_printk_service);
+  
+  lock_init(&status.lock);
   status.char_rq.type = DEV_CHAR_WRITE;
   dev_char_rq_init(&status.char_rq, console_printk_done);
 
